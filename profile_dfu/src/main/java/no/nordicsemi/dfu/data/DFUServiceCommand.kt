0078378e6c8e9db8e07e51cc91fae899f429b03e
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-02-23 16:49:02 +0100

    Remove DFU profile

--- profile_dfu/src/main/java/no/nordicsemi/dfu/data/DFUServiceCommand.kt
+++
@@ -1,3 +0,0 @@
-package no.nordicsemi.dfu.data
-
-internal object DisconnectCommand
--- profile_dfu/src/main/java/no/nordicsemi/dfu/view/DFUState.kt
+++
@@ -1,9 +0,0 @@
-package no.nordicsemi.dfu.view
-
-import no.nordicsemi.dfu.data.DFUData
-
-internal sealed class DFUViewState
-
-internal object LoadingState : DFUViewState()
-
-internal data class DisplayDataState(val data: DFUData) : DFUViewState()
