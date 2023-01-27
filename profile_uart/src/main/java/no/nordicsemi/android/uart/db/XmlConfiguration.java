468e437c25c30db2125f05cc75de25253ca5b2a7
Sylwester Zielinski <sylwester.zielinski@nordicsemi.no>
2023-01-27 13:20:48 +0100

    Revert migration to Kotlin of simpleframework classes as kt version doesn't work.

--- profile_uart/src/main/java/no/nordicsemi/android/uart/db/XmlConfiguration.java
+++ profile_uart/src/main/java/no/nordicsemi/android/uart/db/XmlConfiguration.java
@@ -28,41 +28,57 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package no.nordicsemi.android.uart.db
 
-import org.simpleframework.xml.Attribute
-import org.simpleframework.xml.ElementArray
-import org.simpleframework.xml.Root
-import org.simpleframework.xml.core.PersistenceException
-import org.simpleframework.xml.core.Validate
+package no.nordicsemi.android.uart.db;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.ElementArray;
+import org.simpleframework.xml.Root;
+import org.simpleframework.xml.core.PersistenceException;
+import org.simpleframework.xml.core.Validate;
 
 @Root
-class XmlConfiguration {
-    /**
-     * Returns the field name
-     *
-     * @return optional name
-     */
-    /**
-     * Sets the name to specified value
-     * @param name the new name
-     */
-    @field:Attribute(required = false, empty = "Unnamed")
-    var name: String? = null
+public class XmlConfiguration {
+	public static final int COMMANDS_COUNT = 9;
 
-    /**
-     * Returns the array of commands. There is always 9 of them.
-     * @return the commands array
-     */
-    @field:ElementArray
-    var commands: Array<XmlMacro?> = arrayOfNulls(COMMANDS_COUNT)
-    @Validate
-    @Throws(PersistenceException::class)
-    private fun validate() {
-        if (commands.size != COMMANDS_COUNT) throw PersistenceException("There must be always $COMMANDS_COUNT commands in a configuration.")
-    }
+	@Attribute(required = false, empty = "Unnamed")
+	private String name;
 
-    companion object {
-        const val COMMANDS_COUNT = 9
-    }
+	@ElementArray
+	private XmlMacro[] commands = new XmlMacro[COMMANDS_COUNT];
+
+	/**
+	 * Returns the field name
+	 *
+	 * @return optional name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets the name to specified value
+	 * @param name the new name
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Returns the array of commands. There is always 9 of them.
+	 * @return the commands array
+	 */
+	public XmlMacro[] getCommands() {
+		return commands;
+	}
+
+	public void setCommands(XmlMacro[] commands) {
+		this.commands = commands;
+	}
+
+	@Validate
+	private void validate() throws PersistenceException{
+		if (commands == null || commands.length != COMMANDS_COUNT)
+			throw new PersistenceException("There must be always " + COMMANDS_COUNT + " commands in a configuration.");
+	}
 }
