757658d5736f059ce317a7128521ca51ba18200a
Sylwester Zielinski <sylwester.zielinski@nordicsemi.no>
2023-02-02 12:51:22 +0100

    Update version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -49,7 +49,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:1.2.3")
+            from("no.nordicsemi.android.gradle:version-catalog:1.2.6")
         }
     }
 }
