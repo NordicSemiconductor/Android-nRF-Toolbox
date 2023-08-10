dda63c8de866c2ea6314a661b99e4fb49ffd280d
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2023-08-10 15:23:11 +0200

    Bump version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,7 +50,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:1.9.0")
+            from("no.nordicsemi.android.gradle:version-catalog:1.9.1")
         }
     }
 }
@@ -75,10 +75,10 @@ include(":lib_service")
 include(":lib_ui")
 include(":lib_utils")
 
-if (file("../Android-Common-Libraries").exists()) {
-    includeBuild("../Android-Common-Libraries")
-}
-
-if (file("../Kotlin-BLE-Library").exists()) {
-    includeBuild("../Kotlin-BLE-Library")
-}
+//if (file("../Android-Common-Libraries").exists()) {
+//    includeBuild("../Android-Common-Libraries")
+//}
+//
+//if (file("../Kotlin-BLE-Library").exists()) {
+//    includeBuild("../Kotlin-BLE-Library")
+//}
