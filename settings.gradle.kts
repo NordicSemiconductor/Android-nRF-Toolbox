a253906cf9edbf1ff826e748a730f9c3f42bb602
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2024-01-04 12:41:19 +0100

    Bump version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,7 +50,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:1.11.0")
+            from("no.nordicsemi.android.gradle:version-catalog:1.11.1")
         }
     }
 }
