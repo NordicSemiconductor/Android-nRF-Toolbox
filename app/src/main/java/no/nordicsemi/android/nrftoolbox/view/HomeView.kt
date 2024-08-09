59623e60387136334d4693d11ecb62f562e38004
himalia416 <himali.aryal@nordicsemi.no>
2024-08-09 16:09:42 +0200

    Fix padding

--- app/src/main/java/no/nordicsemi/android/nrftoolbox/view/HomeView.kt
+++ app/src/main/java/no/nordicsemi/android/nrftoolbox/view/HomeView.kt
@@ -1,6 +1,5 @@
 package no.nordicsemi.android.nrftoolbox.view
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,11 +7,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import no.nordicsemi.android.common.permissions.ble.RequireBluetooth
@@ -25,32 +23,27 @@ internal fun HomeView() {
     val viewModel = hiltViewModel<HomeViewModel>()
     val state by viewModel.viewState.collectAsStateWithLifecycle()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+    Scaffold(
+        topBar = {
+            TitleAppBar(stringResource(id = R.string.app_name), state.isScanning)
+        }
     ) {
         RequireBluetooth {
             RequireLocation { isLocationRequiredAndDisabled ->
                 // Both Bluetooth and Location permissions are granted.
                 // We can now start scanning.
-                Scaffold(
-                    topBar = {
-                        TitleAppBar(stringResource(id = R.string.app_name), state.isScanning)
-                    }
+                Column(
+                    modifier = Modifier
+                        .padding(it)
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState())
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(it)
-                            .fillMaxSize()
-                            .verticalScroll(rememberScrollState())
-                    ) {
-                        state.devices.forEach { device ->
-                            ScannedDeviceRow(device = device) {
-                                viewModel.connect(device)
-                            }
+                    LaunchedEffect(key1 = isLocationRequiredAndDisabled) {
+                        viewModel.startScanning()
+                    }
+                    state.devices.forEach { device ->
+                        ScannedDeviceRow(device = device) {
+                            viewModel.connect(device)
                         }
                     }
                 }
