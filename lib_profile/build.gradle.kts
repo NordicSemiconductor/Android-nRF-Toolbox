0427dbb8348c36a56f246ee88e074c14a67a3e2d
hiar <aryal.himali@gmail.com>
2024-08-19 12:47:55 +0200

    Added new module to separate device connection.

---
+++ lib_profile/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.nordic.feature)
+}
+
+android {
+    namespace = "no.nordicsemi.android.toolbox.lib.profile"
+}
+
+dependencies {
+    implementation("no.nordicsemi.kotlin.ble:client-android")
+    implementation(libs.slf4j.timber)
+}
