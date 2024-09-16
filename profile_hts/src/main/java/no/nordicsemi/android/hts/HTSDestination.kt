dfa4dfc8e57a0b9b312138121e2d7af2209c686b
hiar <aryal.himali@gmail.com>
2024-09-16 13:31:34 +0200

    Removed file

--- profile_hts/src/main/java/no/nordicsemi/android/hts/HTSDestination.kt
+++
@@ -1,9 +0,0 @@
-package no.nordicsemi.android.hts
-
-import no.nordicsemi.android.common.navigation.createDestination
-import no.nordicsemi.android.common.navigation.defineDestination
-import no.nordicsemi.android.hts.view.HtsHomeScreen
-
-val HTSDestinationId = createDestination<String, Unit>("hts-destination")
-
-val HTSDestination = defineDestination(HTSDestinationId) { HtsHomeScreen() }
