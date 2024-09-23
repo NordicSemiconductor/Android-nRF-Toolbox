d23937501e0942988af742f21e3b9b32cee646e2
hiar <aryal.himali@gmail.com>
2024-09-23 15:28:20 +0200

    Extracted battery level flow from profile handlers

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/BatteryHandler.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/BatteryHandler.kt
@@ -1,7 +1,6 @@
 package no.nordicsemi.android.toolbox.libs.profile.handler
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.catch
@@ -9,24 +8,18 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
 import no.nordicsemi.android.toolbox.libs.profile.data.hts.data.BatteryLevelParser
-import no.nordicsemi.android.toolbox.libs.profile.spec.ProfileModule
 import no.nordicsemi.kotlin.ble.client.RemoteService
-import no.nordicsemi.kotlin.ble.core.ConnectionState
 import timber.log.Timber
 import java.util.UUID
 
 val BATTERY_LEVEL_CHARACTERISTIC_UUID: UUID =
     UUID.fromString("00002A19-0000-1000-8000-00805f9b34fb")
 
-
-class BatteryHandler : ProfileHandler() {
-    override val profileModule: ProfileModule = ProfileModule.BATTERY
-    override val connectionState: Flow<ConnectionState>
-        get() = MutableSharedFlow()
+class BatteryHandler {
     private val _batteryLevel = MutableSharedFlow<Int>()
-    override fun observeData() = _batteryLevel.asSharedFlow()
+    val batteryLevelData = _batteryLevel.asSharedFlow()
 
-    override suspend fun handleServices(remoteService: RemoteService, scope: CoroutineScope) {
+    suspend fun handleServices(remoteService: RemoteService, scope: CoroutineScope) {
         remoteService.characteristics.firstOrNull { it.uuid == BATTERY_LEVEL_CHARACTERISTIC_UUID }
             ?.subscribe()
             ?.mapNotNull { BatteryLevelParser.parse(it) }
--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/ProfileHandlerFactory.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/ProfileHandlerFactory.kt
@@ -1,6 +1,5 @@
 package no.nordicsemi.android.toolbox.libs.profile.handler
 
-import no.nordicsemi.android.toolbox.libs.profile.spec.BATTERY_SERVICE_UUID
 import no.nordicsemi.android.toolbox.libs.profile.spec.HRS_SERVICE_UUID
 import no.nordicsemi.android.toolbox.libs.profile.spec.HTS_SERVICE_UUID
 import java.util.UUID
@@ -9,7 +8,6 @@ object ProfileHandlerFactory {
     fun createHandler(serviceUuid: UUID): ProfileHandler? {
         return when (serviceUuid) {
             HTS_SERVICE_UUID -> HtsHandler()
-            BATTERY_SERVICE_UUID -> BatteryHandler()
             HRS_SERVICE_UUID -> HrsHandler()
             // Add more service handlers as needed
             else -> null
