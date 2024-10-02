24933f5177e6f08e1aa75614d23e1c10ef9afa30
hiar <aryal.himali@gmail.com>
2024-10-02 14:41:50 +0200

    Added body sensor data

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/HrsHandler.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/HrsHandler.kt
@@ -1,7 +1,6 @@
 package no.nordicsemi.android.toolbox.libs.profile.handler
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.catch
@@ -9,10 +8,10 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
 import no.nordicsemi.android.toolbox.libs.profile.data.Profile
+import no.nordicsemi.android.toolbox.libs.profile.data.hrs.BodySensorLocationParser
 import no.nordicsemi.android.toolbox.libs.profile.data.hrs.HRSData
 import no.nordicsemi.android.toolbox.libs.profile.data.hrs.HRSDataParser
 import no.nordicsemi.kotlin.ble.client.RemoteService
-import no.nordicsemi.kotlin.ble.core.ConnectionState
 import timber.log.Timber
 import java.util.UUID
 
@@ -23,11 +22,12 @@ private val HEART_RATE_MEASUREMENT_CHARACTERISTIC_UUID: UUID =
 
 class HrsHandler : ProfileHandler() {
     override val profile: Profile = Profile.HRS
-    override val connectionState: Flow<ConnectionState>
-        get() = MutableSharedFlow()
-
     private val _hrsData = MutableSharedFlow<HRSData>()
-    override fun observeData() = _hrsData.asSharedFlow()
+    private var _bodySensorLocation: Int? = null
+
+    override fun getNotification() = _hrsData.asSharedFlow()
+
+    override fun readCharacteristic() = _bodySensorLocation
 
     override suspend fun handleServices(remoteService: RemoteService, scope: CoroutineScope) {
         remoteService.characteristics.firstOrNull { it.uuid == HEART_RATE_MEASUREMENT_CHARACTERISTIC_UUID }
@@ -41,5 +41,14 @@ class HrsHandler : ProfileHandler() {
                 e.printStackTrace()
                 Timber.e(e)
             }?.launchIn(scope)
+
+        remoteService.characteristics.firstOrNull { it.uuid == BODY_SENSOR_LOCATION_CHARACTERISTIC_UUID }
+            ?.read()
+            ?.let { BodySensorLocationParser.parse(it) }
+            ?.let { bodySensorLocation ->
+                // Handle the body sensor location
+                _bodySensorLocation = bodySensorLocation
+
+            }
     }
 }
--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/ProfileHandler.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/handler/ProfileHandler.kt
@@ -4,11 +4,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import no.nordicsemi.android.toolbox.libs.profile.data.Profile
 import no.nordicsemi.kotlin.ble.client.RemoteService
-import no.nordicsemi.kotlin.ble.core.ConnectionState
 
 abstract class ProfileHandler {
     abstract val profile: Profile
-    abstract val connectionState: Flow<ConnectionState>
-    abstract fun observeData(): Flow<Any>
+    abstract fun getNotification(): Flow<Any>
+    abstract fun readCharacteristic(): Any?
     abstract suspend fun handleServices(remoteService: RemoteService, scope: CoroutineScope)
 }
