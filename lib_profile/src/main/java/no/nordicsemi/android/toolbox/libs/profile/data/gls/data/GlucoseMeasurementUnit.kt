f39e0681a076eec88f7f29a9aafa6127f58b8b82
hiar <aryal.himali@gmail.com>
2024-10-21 14:52:15 +0200

    Added glucose monitoring data.

---
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/data/gls/data/GlucoseMeasurementUnit.kt
@@ -0,0 +1,6 @@
+package no.nordicsemi.android.toolbox.libs.profile.data.gls.data
+
+enum class GlucoseMeasurementUnit {
+    UNIT_mol_L,
+    UNIT_kg_L
+}
---
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/data/racp/RACPResponseCode.kt
@@ -0,0 +1,21 @@
+package no.nordicsemi.android.toolbox.libs.profile.data.racp
+
+enum class RACPResponseCode(internal val value: Int) {
+    RACP_RESPONSE_SUCCESS(1),
+    RACP_ERROR_OP_CODE_NOT_SUPPORTED(2),
+    RACP_ERROR_INVALID_OPERATOR(3),
+    RACP_ERROR_OPERATOR_NOT_SUPPORTED(4),
+    RACP_ERROR_INVALID_OPERAND(5),
+
+    RACP_ERROR_NO_RECORDS_FOUND(6),
+    RACP_ERROR_ABORT_UNSUCCESSFUL(7),
+    RACP_ERROR_PROCEDURE_NOT_COMPLETED(8),
+    RACP_ERROR_OPERAND_NOT_SUPPORTED(9);
+
+    companion object {
+        fun create(value: Int): RACPResponseCode {
+            return entries.firstOrNull { it.value == value }
+                ?: throw IllegalArgumentException("Cannot create RACP response code for value: $value")
+        }
+    }
+}
