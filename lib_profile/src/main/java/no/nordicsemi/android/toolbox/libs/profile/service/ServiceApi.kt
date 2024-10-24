0b149833b4e86afb6325cb1c6eb1ef6d4d0e3746
hiar <aryal.himali@gmail.com>
2024-10-24 09:59:04 +0200

    minor changes

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/service/ServiceApi.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/service/ServiceApi.kt
@@ -12,7 +12,7 @@ sealed interface DeviceDisconnectionReason
 /** Includes the [ConnectionState.Disconnected.Reason]. */
 data class StateReason(val reason: ConnectionState.Disconnected.Reason) : DeviceDisconnectionReason
 
-/** Includes the custom made [DisconnectReason] to include other disconnection reason which are not included in the [ConnectionState.Disconnected.Reason]. */
+/** Includes the custom made [DisconnectReason] to include other disconnection reasons which are not included in the [ConnectionState.Disconnected.Reason]. */
 data class CustomReason(val reason: DisconnectReason) :
     DeviceDisconnectionReason
 
@@ -25,21 +25,21 @@ interface ServiceApi {
     val isMissingServices: Flow<Boolean>
 
     /**
-     * Get the peripheral by its address.
-     * @param address the device address.
+     * Get the peripheral by its [address].
+     *
      * @return the peripheral instance.
      */
     fun getPeripheralById(address: String?): Peripheral?
 
     /**
-     * Disconnect the device with the given address.
+     * Disconnect the device with the given [deviceAddress].
      *
      * @param deviceAddress the device address.
      */
     fun disconnect(deviceAddress: String)
 
     /**
-     * Get the connection state of the device with the given address.
+     * Get the connection state of the device with the given [address].
      *
      * @return the connection state flow.
      */
