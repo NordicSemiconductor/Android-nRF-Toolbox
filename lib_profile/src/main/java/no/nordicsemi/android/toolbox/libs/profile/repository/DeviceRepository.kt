05598dcd00ca4531d3cbb584b28f52d9fbe0b2e3
hiar <aryal.himali@gmail.com>
2024-11-06 15:12:41 +0100

    Changed to update

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/repository/DeviceRepository.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/repository/DeviceRepository.kt
@@ -2,6 +2,7 @@ package no.nordicsemi.android.toolbox.libs.profile.repository
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import no.nordicsemi.android.service.handler.ServiceHandler
 import no.nordicsemi.kotlin.ble.client.android.Peripheral
 import javax.inject.Inject
@@ -9,10 +10,11 @@ import javax.inject.Singleton
 
 @Singleton
 class DeviceRepository @Inject constructor() {
-    private val _connectedDevices = MutableStateFlow<Map<String, Pair<Peripheral, List<ServiceHandler>>>>(emptyMap())
+    private val _connectedDevices =
+        MutableStateFlow<Map<String, Pair<Peripheral, List<ServiceHandler>>>>(emptyMap())
     val connectedDevices = _connectedDevices.asStateFlow()
 
     fun updateConnectedDevices(devices: Map<String, Pair<Peripheral, List<ServiceHandler>>>) {
-        _connectedDevices.value = devices
+        _connectedDevices.update { devices }
     }
 }
