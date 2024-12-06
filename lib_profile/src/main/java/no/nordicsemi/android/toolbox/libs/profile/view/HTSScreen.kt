249b9bbd178fe86f626efa81201a6c432f3cf9c0
hiar <aryal.himali@gmail.com>
2024-12-06 10:38:21 +0100

    Ui fixes.

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/view/HTSScreen.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/view/HTSScreen.kt
@@ -1,13 +1,9 @@
 package no.nordicsemi.android.toolbox.libs.profile.view
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -54,16 +50,5 @@ internal fun HTSScreen(
                 )
             )
         }
-    }
-}
-
-@Composable
-internal fun LoadingView() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        CircularProgressIndicator(
-            modifier = Modifier.align(Alignment.Center)
-        )
     }
 }
