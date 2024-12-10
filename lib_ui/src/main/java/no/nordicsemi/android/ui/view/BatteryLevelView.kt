5df0547835b8993f2af00743fc2b82a1b1394d61
hiar <aryal.himali@gmail.com>
2024-12-10 15:36:32 +0100

    Ui changes battery service

--- lib_ui/src/main/java/no/nordicsemi/android/ui/view/BatteryLevelView.kt
+++ lib_ui/src/main/java/no/nordicsemi/android/ui/view/BatteryLevelView.kt
@@ -31,16 +31,103 @@
 
 package no.nordicsemi.android.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Battery0Bar
+import androidx.compose.material.icons.outlined.Battery1Bar
+import androidx.compose.material.icons.outlined.Battery2Bar
+import androidx.compose.material.icons.outlined.Battery3Bar
+import androidx.compose.material.icons.outlined.Battery4Bar
+import androidx.compose.material.icons.outlined.Battery5Bar
+import androidx.compose.material.icons.outlined.Battery6Bar
+import androidx.compose.material.icons.outlined.BatteryAlert
+import androidx.compose.material.icons.outlined.BatteryFull
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import no.nordicsemi.android.common.theme.nordicGrass
+import no.nordicsemi.android.common.theme.nordicGreen
 import no.nordicsemi.android.ui.R
 
 @Composable
 fun BatteryLevelView(batteryLevel: Int) {
     ScreenSection {
-        KeyValueField(
-            stringResource(id = R.string.field_battery),
-            "$batteryLevel%"
-        )
+        Row(
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(id = R.string.field_battery),
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                DynamicBatteryStatus(batteryLevel)
+                Text(text = "$batteryLevel %")
+            }
+        }
+
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DynamicBatteryStatus(batteryLevel: Int = 40) {
+    val (batteryIcon: ImageVector, color: Color) = when {
+        batteryLevel > 95 -> {
+            Icons.Outlined.BatteryFull to MaterialTheme.colorScheme.nordicGreen
+        } // Full Battery
+        batteryLevel > 80 -> {
+            Icons.Outlined.Battery6Bar to MaterialTheme.colorScheme.nordicGreen
+        }
+
+        batteryLevel > 70 -> {
+            Icons.Outlined.Battery5Bar to MaterialTheme.colorScheme.nordicGreen
+        } // Moderate Battery
+        batteryLevel > 55 -> {
+            Icons.Outlined.Battery4Bar to MaterialTheme.colorScheme.nordicGreen
+        } // Moderate Battery
+
+        batteryLevel > 40 -> {
+            Icons.Outlined.Battery3Bar to MaterialTheme.colorScheme.nordicGreen
+        } // Moderate Battery
+
+        batteryLevel > 25 -> {
+            Icons.Outlined.Battery2Bar to MaterialTheme.colorScheme.nordicGrass
+        } // Low Battery
+
+        batteryLevel > 10 -> {
+            Icons.Outlined.Battery1Bar to MaterialTheme.colorScheme.error
+        }  // Low Battery
+
+        batteryLevel > 5 -> {
+            Icons.Outlined.Battery0Bar to MaterialTheme.colorScheme.error
+        } // Low Battery
+
+        else -> {
+            Icons.Outlined.BatteryAlert to MaterialTheme.colorScheme.error
+        } // Critically Low Battery
+    }
+
+    Icon(
+        imageVector = batteryIcon,
+        contentDescription = "Battery icon",
+        tint = color,
+    )
 }
