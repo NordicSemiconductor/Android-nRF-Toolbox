c5c8c064ca5178da6dbc52453fca792138d8a11f
hiar <aryal.himali@gmail.com>
2024-12-16 14:13:33 +0100

    Removed unused code.

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/data/HTSUiMapper.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/data/HTSUiMapper.kt
@@ -31,49 +31,13 @@
 
 package no.nordicsemi.android.toolbox.libs.profile.data
 
-import no.nordicsemi.android.common.ui.view.RadioButtonItem
-import no.nordicsemi.android.common.ui.view.RadioGroupViewEntity
 import no.nordicsemi.android.toolbox.libs.core.data.hts.TemperatureUnit
 import java.util.Locale
-
-private const val DISPLAY_FAHRENHEIT = "°F"
-private const val DISPLAY_CELSIUS = "°C"
-private const val DISPLAY_KELVIN = "°K"
 
 internal fun displayTemperature(value: Float, temperatureUnit: TemperatureUnit): String {
     return when (temperatureUnit) {
         TemperatureUnit.CELSIUS -> String.format(Locale.US, "%.1f °C", value)
         TemperatureUnit.FAHRENHEIT -> String.format(Locale.US, "%.1f °F", value * 1.8f + 32f)
         TemperatureUnit.KELVIN -> String.format(Locale.US, "%.1f °K", value + 273.15f)
-    }
-}
-
-internal fun String.toTemperatureUnit(): TemperatureUnit {
-    return when (this) {
-        DISPLAY_CELSIUS -> TemperatureUnit.CELSIUS
-        DISPLAY_FAHRENHEIT -> TemperatureUnit.FAHRENHEIT
-        DISPLAY_KELVIN -> TemperatureUnit.KELVIN
-        else -> throw IllegalArgumentException("Can't create TemperatureUnit from this label: $this")
-    }
-}
-
-internal fun TemperatureUnit.temperatureSettingsItems(): RadioGroupViewEntity {
-    return RadioGroupViewEntity(
-        TemperatureUnit.entries.map { createRadioButtonItem(it, this) }
-    )
-}
-
-private fun createRadioButtonItem(
-    unit: TemperatureUnit,
-    selectedTemperatureUnit: TemperatureUnit
-): RadioButtonItem {
-    return RadioButtonItem(displayTemperature(unit), unit == selectedTemperatureUnit)
-}
-
-private fun displayTemperature(unit: TemperatureUnit): String {
-    return when (unit) {
-        TemperatureUnit.CELSIUS -> DISPLAY_CELSIUS
-        TemperatureUnit.FAHRENHEIT -> DISPLAY_FAHRENHEIT
-        TemperatureUnit.KELVIN -> DISPLAY_KELVIN
     }
 }
