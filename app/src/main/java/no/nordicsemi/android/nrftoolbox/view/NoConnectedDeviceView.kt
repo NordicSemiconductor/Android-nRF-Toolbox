8f5246e63100ae6be2b19c696a6f00c7f3d8f8eb
hiar <aryal.himali@gmail.com>
2025-01-08 09:21:18 +0100

    Home view changes

--- app/src/main/java/no/nordicsemi/android/nrftoolbox/view/NoConnectedDeviceView.kt
+++ app/src/main/java/no/nordicsemi/android/nrftoolbox/view/NoConnectedDeviceView.kt
@@ -1,30 +1,48 @@
 package no.nordicsemi.android.nrftoolbox.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import no.nordicsemi.android.common.core.parseBold
 import no.nordicsemi.android.common.theme.NordicTheme
-import no.nordicsemi.android.common.ui.view.WarningView
 import no.nordicsemi.android.nrftoolbox.R
 
 @Composable
 internal fun NoConnectedDeviceView() {
-    WarningView(
+
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
-        painterResource = painterResource(id = R.drawable.ic_notification_icon),
-        title = "CAN'T SEE CONNECTED DEVICES?",
-        hint = "1. Make sure the device is turned on and is connected to a power source.\n\n" +
-                "2. Make sure the device is in the range of your phone.\n\n".parseBold(),
-        hintTextAlign = TextAlign.Justify,
-    )
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_notification_icon),
+            contentDescription = null,
+            modifier = Modifier.size(80.dp),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+        )
+
+        Text(text = "CAN'T SEE CONNECTED DEVICES?")
+        Text(
+            text = "1. Make sure the device is turned on and is connected to a power source.\n\n" +
+                    "2. Make sure the device is in the range of your phone.\n\n",
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
