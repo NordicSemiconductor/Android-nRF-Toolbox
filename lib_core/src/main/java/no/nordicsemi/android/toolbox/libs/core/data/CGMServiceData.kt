6616b853716932e0d1db4e5aa8f5b67df0a37d84
hiar <aryal.himali@gmail.com>
2025-01-08 16:17:37 +0100

    Updated cgm view

--- lib_core/src/main/java/no/nordicsemi/android/toolbox/libs/core/data/CGMServiceData.kt
+++ lib_core/src/main/java/no/nordicsemi/android/toolbox/libs/core/data/CGMServiceData.kt
@@ -2,12 +2,14 @@ package no.nordicsemi.android.toolbox.libs.core.data
 
 import no.nordicsemi.android.toolbox.libs.core.Profile
 import no.nordicsemi.android.toolbox.libs.core.data.cgms.data.CGMRecord
+import no.nordicsemi.android.toolbox.libs.core.data.common.WorkingMode
 import no.nordicsemi.android.toolbox.libs.core.data.gls.data.RequestStatus
 
 data class CGMServiceData(
     override val profile: Profile = Profile.CGM,
     val records: List<CGMRecordWithSequenceNumber> = emptyList(),
     val requestStatus: RequestStatus = RequestStatus.IDLE,
+    val workingMode: WorkingMode? = null,
 ) : ProfileServiceData()
 
 data class CGMRecordWithSequenceNumber(
