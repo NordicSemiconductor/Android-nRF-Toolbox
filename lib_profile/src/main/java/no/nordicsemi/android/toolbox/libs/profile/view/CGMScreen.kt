f94eb665dad74a866cfb6b47e165b480f944b81e
hiar <aryal.himali@gmail.com>
2025-01-09 12:09:42 +0100

    Fixed cgm ui

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/view/CGMScreen.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/view/CGMScreen.kt
@@ -1,21 +1,35 @@
 package no.nordicsemi.android.toolbox.libs.profile.view
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +44,10 @@ import no.nordicsemi.android.toolbox.libs.profile.data.glucoseConcentration
 import no.nordicsemi.android.toolbox.libs.profile.data.toDisplayString
 import no.nordicsemi.android.toolbox.libs.profile.viewmodel.DeviceConnectionViewEvent
 import no.nordicsemi.android.toolbox.libs.profile.viewmodel.GLSViewEvent.OnWorkingModeSelected
+import no.nordicsemi.android.ui.view.KeyValueColumn
+import no.nordicsemi.android.ui.view.KeyValueColumnReverse
 import no.nordicsemi.android.ui.view.ScreenSection
+import no.nordicsemi.android.ui.view.SectionRow
 import no.nordicsemi.android.ui.view.SectionTitle
 
 @Composable
@@ -80,7 +97,7 @@ private fun WorkingModeDropDown(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text(text = "Working mode")
+                    Text(text = "Request")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "")
                 }
             }
@@ -190,15 +207,21 @@ private fun RecordsViewWithoutDataPreview() {
 @Composable
 private fun RecordsViewWithData(state: CGMServiceData) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier.fillMaxWidth()
     ) {
         SectionTitle(resId = R.drawable.ic_records, title = "Records")
+        val newRecord = when (state.workingMode) {
+            WorkingMode.ALL -> state.records
+            WorkingMode.LAST -> listOf(state.records.last())
+            WorkingMode.FIRST -> listOf(state.records.first())
+            null -> state.records
+        }
 
-        state.records.forEachIndexed { i, it ->
+        newRecord.forEachIndexed { i, it ->
             RecordItem(it)
 
-            if (i < state.records.size - 1) {
+            if (i < newRecord.size - 1) {
+                HorizontalDivider()
                 Spacer(modifier = Modifier.size(8.dp))
             }
         }
@@ -207,27 +230,36 @@ private fun RecordsViewWithData(state: CGMServiceData) {
 
 @Composable
 private fun RecordItem(record: CGMRecordWithSequenceNumber) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
+            .padding(8.dp)
+    ) {
         Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
         ) {
-            Text(
-                text = record.formattedTime(),
-                style = MaterialTheme.typography.labelLarge
-            )
+            SectionRow {
+                KeyValueColumn(
+                    value = stringResource(id = R.string.cgms_sequence_number),
+                    key = record.sequenceNumber.toString()
+                )
+                KeyValueColumnReverse(
+                    value = "Date/Time",
+                    key = record.formattedTime()
+                )
+            }
+            SectionRow {
+                KeyValueColumn(
+                    "Glucose concentration",
+                    record.glucoseConcentration()
+                )
+            }
 
-            Text(
-                text = stringResource(id = R.string.cgms_sequence_number, record.sequenceNumber),
-                style = MaterialTheme.typography.bodyMedium
-            )
         }
-
-        Text(
-            text = record.glucoseConcentration(),
-            style = MaterialTheme.typography.titleMedium,
-        )
     }
 }
 
