3d8bf6e211ac1c369ea99c6d9a2851e4dc9c2cb7
hiar <aryal.himali@gmail.com>
2025-01-13 15:55:47 +0100

    Changed view.

--- lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/view/BPSScreen.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/toolbox/libs/profile/view/BPSScreen.kt
@@ -3,6 +3,7 @@ package no.nordicsemi.android.toolbox.libs.profile.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,12 +12,20 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import no.nordicsemi.android.toolbox.lib.profile.R
-import no.nordicsemi.android.toolbox.libs.core.data.bps.BloodPressureMeasurementData
-import no.nordicsemi.android.toolbox.libs.core.data.bps.IntermediateCuffPressureData
+import no.nordicsemi.android.toolbox.libs.core.Profile
 import no.nordicsemi.android.toolbox.libs.core.data.BPSServiceData
-import no.nordicsemi.android.ui.view.KeyValueField
+import no.nordicsemi.android.toolbox.libs.core.data.bps.BPMStatus
+import no.nordicsemi.android.toolbox.libs.core.data.bps.BloodPressureMeasurementData
+import no.nordicsemi.android.toolbox.libs.core.data.bps.BloodPressureType
+import no.nordicsemi.android.toolbox.libs.core.data.bps.IntermediateCuffPressureData
+import no.nordicsemi.android.ui.view.KeyValueColumn
+import no.nordicsemi.android.ui.view.KeyValueColumnReverse
 import no.nordicsemi.android.ui.view.ScreenSection
+import no.nordicsemi.android.ui.view.SectionRow
 import no.nordicsemi.android.ui.view.SectionTitle
+import no.nordicsemi.android.ui.view.dialog.toBooleanText
+import java.util.Calendar
+
 
 @Composable
 internal fun BPSScreen(
@@ -28,8 +37,8 @@ internal fun BPSScreen(
     ) {
         ScreenSection {
             SectionTitle(
-                resId = R.drawable.ic_records,
-                title = stringResource(id = R.string.bps_records)
+                resId = R.drawable.ic_bps,
+                title = stringResource(id = R.string.bps_title)
             )
 
             serviceData.bloodPressureMeasurement?.let {
@@ -37,7 +46,10 @@ internal fun BPSScreen(
             }
 
             serviceData.intermediateCuffPressure?.displayHeartRate()?.let {
-                KeyValueField(stringResource(id = R.string.bps_pulse), it)
+                HorizontalDivider()
+                SectionRow {
+                    KeyValueColumn(stringResource(id = R.string.bps_pulse), it)
+                }
             }
 
             if (serviceData.intermediateCuffPressure == null && serviceData.bloodPressureMeasurement == null) {
@@ -50,34 +62,150 @@ internal fun BPSScreen(
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun BPSScreenPreview() {
+    val sampleStatus = BPMStatus(
+        bodyMovementDetected = true,
+        cuffTooLose = false,
+        irregularPulseDetected = true,
+        pulseRateInRange = false,
+        pulseRateExceedsUpperLimit = true,
+        pulseRateIsLessThenLowerLimit = false,
+        improperMeasurementPosition = true
+    )
+
+    val sampleData = BloodPressureMeasurementData(
+        systolic = 12.0f,
+        diastolic = 15.0f,
+        meanArterialPressure = 10.0f,
+        unit = BloodPressureType.UNIT_MMHG,
+        pulseRate = 12.0f,
+        userID = 15,
+        status = sampleStatus,
+        calendar = Calendar.getInstance()
+    )
+    BPSScreen(
+        serviceData = BPSServiceData(
+            profile = Profile.BPS,
+            bloodPressureMeasurement = sampleData,
+        )
+    )
+}
+
 @Composable
 private fun BloodPressureView(state: BloodPressureMeasurementData) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(4.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        KeyValueField(stringResource(id = R.string.bps_systolic), state.displaySystolic())
-        KeyValueField(stringResource(id = R.string.bps_diastolic), state.displayDiastolic())
-        KeyValueField(stringResource(id = R.string.bps_mean), state.displayMeanArterialPressure())
+        SectionRow {
+            KeyValueColumn(
+                stringResource(id = R.string.bps_systolic),
+                state.displaySystolic()
+            )
+            KeyValueColumnReverse(
+                stringResource(id = R.string.bps_diastolic),
+                state.displayDiastolic()
+            )
+        }
+        SectionRow {
+            KeyValueColumn(
+                stringResource(id = R.string.bps_mean),
+                state.displayMeanArterialPressure()
+            )
+            state.pulseRate?.let {
+                KeyValueColumnReverse(
+                    "Pulse", state.displayPulseRate()
+                )
+            }
+        }
+        SectionRow {
+            state.calendar?.let {
+                stringResource(R.string.bps_timestamp, it)
+            }?.let {
+                KeyValueColumn(
+                    "Date/Time",
+                    it
+                )
+            }
+            state.status?.bodyMovementDetected?.let {
+                KeyValueColumnReverse(
+                    "Body movement detected",
+                    it.toBooleanText()
+                )
+            }
+        }
+
+        state.status?.irregularPulseDetected?.let {
+            KeyValueColumn(
+                "Irregular pulse detected",
+                it.toBooleanText()
+            )
+        }
+        state.status?.cuffTooLose?.let {
+            KeyValueColumn(
+                "Cuff Too Lose",
+                it.toBooleanText()
+            )
+        }
+
+
+        state.status?.pulseRateExceedsUpperLimit?.let {
+            KeyValueColumn(
+                "Pulse rate exceeds upper limit",
+                it.toBooleanText()
+            )
+        }
+        state.status?.pulseRateInRange?.let {
+            KeyValueColumn(
+                "Pulse rate in range",
+                it.toBooleanText()
+            )
+        }
+        state.status?.improperMeasurementPosition?.let {
+            KeyValueColumn(
+                "Improper measurement position",
+                it.toBooleanText()
+            )
+        }
+
+        state.status?.pulseRateIsLessThenLowerLimit?.let {
+            KeyValueColumn(
+                "Pulse rate less than lower limit",
+                it.toBooleanText()
+            )
+        }
+
     }
 }
 
 @Composable
 fun BloodPressureMeasurementData.displaySystolic(): String =
-    stringResource(id = R.string.bps_blood_pressure, systolic)
+    stringResource(
+        id = R.string.bps_blood_pressure,
+        systolic, displayUnit()
+    )
 
 @Composable
 fun BloodPressureMeasurementData.displayDiastolic(): String =
-    stringResource(id = R.string.bps_blood_pressure, diastolic)
+    stringResource(
+        id = R.string.bps_blood_pressure,
+        diastolic, displayUnit()
+    )
 
 @Composable
 fun BloodPressureMeasurementData.displayMeanArterialPressure(): String =
-    stringResource(id = R.string.bps_blood_pressure, meanArterialPressure)
+    stringResource(
+        id = R.string.bps_blood_pressure,
+        meanArterialPressure, displayUnit()
+    )
 
 @Composable
-fun IntermediateCuffPressureData.displayHeartRate(): String? = pulseRate?.toString()
+fun IntermediateCuffPressureData.displayHeartRate(): String = pulseRate?.toString() + " bpm"
 
-@Preview
 @Composable
-private fun Preview() {
-    BPSScreen(BPSServiceData())
-}
+fun BloodPressureMeasurementData.displayPulseRate(): String = pulseRate?.toString() + " bpm"
+
+@Composable
+fun BloodPressureMeasurementData.displayUnit(): String =
+    if (unit == BloodPressureType.UNIT_MMHG) "mmGH" else "kPA"
