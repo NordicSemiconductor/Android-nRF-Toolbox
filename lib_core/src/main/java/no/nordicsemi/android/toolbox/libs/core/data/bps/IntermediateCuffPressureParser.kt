94cfa958bfd23f80d2d6f4466f81f169c26b0e4d
hiar <aryal.himali@gmail.com>
2025-01-14 13:55:13 +0100

    Intermediate CuffPressure Parser and its test cases.

--- lib_core/src/main/java/no/nordicsemi/android/toolbox/libs/core/data/bps/IntermediateCuffPressureParser.kt
+++ lib_core/src/main/java/no/nordicsemi/android/toolbox/libs/core/data/bps/IntermediateCuffPressureParser.kt
@@ -1,25 +1,27 @@
 package no.nordicsemi.android.toolbox.libs.core.data.bps
 
-import no.nordicsemi.android.kotlin.ble.core.data.util.DataByteArray
-import no.nordicsemi.android.kotlin.ble.core.data.util.FloatFormat
-import no.nordicsemi.android.kotlin.ble.core.data.util.IntFormat
 import no.nordicsemi.android.toolbox.libs.core.data.hts.DateTimeParser
+import no.nordicsemi.kotlin.data.FloatFormat
+import no.nordicsemi.kotlin.data.IntFormat
+import no.nordicsemi.kotlin.data.getFloat
+import no.nordicsemi.kotlin.data.getInt
+import java.nio.ByteOrder
 import java.util.Calendar
 
 object IntermediateCuffPressureParser {
 
-    fun parse(data: ByteArray): IntermediateCuffPressureData? {
-        val bytes = DataByteArray(data)
-        if (bytes.size < 7) {
-            return null
-        }
+    fun parse(
+        data: ByteArray,
+        byteOrder: ByteOrder = ByteOrder.LITTLE_ENDIAN
+    ): IntermediateCuffPressureData? {
+        if (data.size < 7) return null
 
         // First byte: flags
         var offset = 0
-        val flags: Int = bytes.getIntValue(IntFormat.FORMAT_UINT8, offset++) ?: return null
+        val flags: Int = data.getInt(offset++, IntFormat.UINT8)
 
         // See UNIT_* for unit options
-        val unit: BloodPressureType = if (flags and 0x01 == BloodPressureType.UNIT_MMHG.value) {
+        val unit = if (flags and 0x01 == 0) {
             BloodPressureType.UNIT_MMHG
         } else {
             BloodPressureType.UNIT_KPA
@@ -29,7 +31,7 @@ object IntermediateCuffPressureParser {
         val userIdPresent = flags and 0x08 != 0
         val measurementStatusPresent = flags and 0x10 != 0
 
-        if (bytes.size < (7
+        if (data.size < (7
                     + (if (timestampPresent) 7 else 0) + (if (pulseRatePresent) 2 else 0)
                     + (if (userIdPresent) 1 else 0) + if (measurementStatusPresent) 2 else 0)
         ) {
@@ -37,36 +39,34 @@ object IntermediateCuffPressureParser {
         }
 
         // Following bytes - systolic, diastolic and mean arterial pressure
-        val cuffPressure: Float = bytes.getFloatValue(FloatFormat.FORMAT_SFLOAT, offset) ?: return null
-        // final float ignored_1 = data.getFloatValue(Data.FORMAT_SFLOAT, offset + 2);
-        // final float ignored_2 = data.getFloatValue(Data.FORMAT_SFLOAT, offset + 4);
+        val cuffPressure: Float = data.getFloat(offset, FloatFormat.IEEE_11073_16_BIT, byteOrder)
         offset += 6
 
         // Parse timestamp if present
         var calendar: Calendar? = null
         if (timestampPresent) {
-            calendar = DateTimeParser.parse(bytes, offset)
+            calendar = DateTimeParser.parse(data, offset)
             offset += 7
         }
 
         // Parse pulse rate if present
         var pulseRate: Float? = null
         if (pulseRatePresent) {
-            pulseRate = bytes.getFloatValue(FloatFormat.FORMAT_SFLOAT, offset)
+            pulseRate = data.getFloat(offset, FloatFormat.IEEE_11073_16_BIT, byteOrder)
             offset += 2
         }
 
         // Read user id if present
         var userId: Int? = null
         if (userIdPresent) {
-            userId = bytes.getIntValue(IntFormat.FORMAT_UINT8, offset)
+            userId = data.getInt(offset, IntFormat.UINT8)
             offset += 1
         }
 
         // Read measurement status if present
         var status: BPMStatus? = null
         if (measurementStatusPresent) {
-            val measurementStatus: Int = bytes.getIntValue(IntFormat.FORMAT_UINT16_LE, offset) ?: return null
+            val measurementStatus: Int = data.getInt(offset, IntFormat.UINT16, byteOrder)
             // offset += 2;
             status = BPMStatus(measurementStatus)
         }
