e61211afac7cee72d5ba5cf6770c0131fd5c4d01
hiar <aryal.himali@gmail.com>
2025-01-22 09:29:45 +0100

    Removed unused dependency.

--- lib_core/build.gradle.kts
+++ lib_core/build.gradle.kts
@@ -9,10 +9,6 @@ android {
 
 dependencies {
     implementation("no.nordicsemi.kotlin.ble:client-android")
-
-    // TODO: This will be removed once the new BLEK library is implemented.
-    implementation(libs.nordic.blek.client)
-
     implementation("no.nordicsemi.kotlin:data:0.3.0")
 
     // Unit test dependencies
