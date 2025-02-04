d02afe95b1fd9a31345c00e750ccd270506cf861
hiar <aryal.himali@gmail.com>
2025-02-04 15:24:49 +0100

    Changed dropdown style.

--- lib_ui/src/main/java/no/nordicsemi/android/ui/view/DropdownView.kt
+++ lib_ui/src/main/java/no/nordicsemi/android/ui/view/DropdownView.kt
@@ -1,45 +1,31 @@
 package no.nordicsemi.android.ui.view
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,84 +40,74 @@ inline fun <reified T> DropdownView(
 ) {
     var expanded by remember { mutableStateOf(false) }
     var selectedText by rememberSaveable { mutableStateOf(defaultSelectedItem) }
-    var selectedIndex by rememberSaveable { mutableIntStateOf(items.indexOfFirst { it == selectedText }) }
 
-    Box(modifier = Modifier.height(IntrinsicSize.Min)) {
-        OutlinedTextField(
-            value = selectedText?.toString() ?: placeholder,
-            onValueChange = { },
-            readOnly = true,
-            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text(text = placeholder) },
-            label = { Text(text = label) },
-            isError = isError,
-            supportingText = {
-                if (isError) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Error,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.error,
-                        )
-                        Text(
-                            text = errorMessage,
-                            color = MaterialTheme.colorScheme.error,
-                            style = MaterialTheme.typography.bodySmall,
-                        )
-                    }
-                }
-            },
-        )
-        // Transparent clickable surface on top of OutlinedTextField
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .clip(MaterialTheme.shapes.extraSmall)
-                .clickable { expanded = true },
-            color = Color.Transparent,
-        ) { }
-    }
-
-    AnimatedVisibility(visible = expanded) {
-        Dialog(onDismissRequest = { expanded = false }) {
-            Surface(shape = RoundedCornerShape(12.dp)) {
-                val listState = rememberLazyListState()
-                if (selectedIndex > -1) {
-                    LaunchedEffect("ScrollToSelected") {
-                        listState.scrollToItem(index = selectedIndex)
-                    }
-                }
-
-                LazyColumn(modifier = Modifier.fillMaxWidth(), state = listState) {
-                    itemsIndexed(items) { index, item ->
-                        val selectedItem = index == selectedIndex
-                        val (backgroundColor, itemColor) = if (selectedItem) {
-                            MaterialTheme.colorScheme.primary to MaterialTheme.colorScheme.onPrimary
-                        } else {
-                            MaterialTheme.colorScheme.surface to MaterialTheme.colorScheme.onSurface
+    Box {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }) {
+            OutlinedTextField(
+                value = selectedText?.toString() ?: placeholder,
+                onValueChange = { }, // No need to handle since it's readOnly
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .menuAnchor(MenuAnchorType.PrimaryNotEditable),
+                placeholder = { Text(text = placeholder) },
+                label = { Text(text = label) },
+                isError = isError,
+                supportingText = {
+                    if (isError) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Error,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.error,
+                            )
+                            Text(
+                                text = errorMessage,
+                                color = MaterialTheme.colorScheme.error,
+                                style = MaterialTheme.typography.bodySmall,
+                            )
                         }
+                    }
+                },
+            )
+            // Animated dropdown menu
+            AnimatedVisibility(visible = expanded) {
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier.exposedDropdownSize(),
+                ) {
+                    items.forEach {
                         DropdownMenuItem(
-                            colors = MenuDefaults.itemColors(itemColor),
-                            modifier = Modifier.background(backgroundColor),
-                            text = { Text(item.toString()) },
+                            text = { Text(it.toString()) },
                             onClick = {
-                                selectedText = item
-                                selectedIndex = index
+                                selectedText = it
                                 expanded = false
-                                onItemSelected(item)
-                            }
+                                onItemSelected(it)
+                            },
                         )
-
-                        if (index < items.lastIndex) {
-                            HorizontalDivider()
-                        }
                     }
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun DropdownViewPreview() {
+    val items = listOf("Item 1", "Item 2", "Item 3")
+    DropdownView(
+        items = items,
+        label = "Label",
+        placeholder = "Placeholder",
+        defaultSelectedItem = items[0],
+        onItemSelected = {}
+    )
 }
