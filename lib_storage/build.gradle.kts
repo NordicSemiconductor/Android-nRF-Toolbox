34eb23d38e40293211875e705e1de864dc726206
hiar <aryal.himali@gmail.com>
2025-02-05 13:03:40 +0100

    Added module for uart database.

---
+++ lib_storage/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.nordic.feature)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+
+}
+
+android {
+    namespace = "no.nordicsemi.android.toolbox.lib.storage"
+}
+
+dependencies {
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+}
