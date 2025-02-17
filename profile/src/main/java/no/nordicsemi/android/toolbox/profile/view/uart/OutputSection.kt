01852af13c56d2e8cbaf2a31062a9b4c353ab423
hiar <aryal.himali@gmail.com>
2025-02-17 14:46:16 +0100

    Show latest messages at the top.

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/OutputSection.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/OutputSection.kt
@@ -9,12 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -23,11 +18,9 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,6 +41,7 @@ internal fun OutputSection(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.padding(16.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -60,45 +54,26 @@ internal fun OutputSection(
                 menu = { Menu(onEvent) }
             )
         }
-        val itemHeight = 56.dp // Approximate height of each item
-        val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-        val maxHeight = screenHeight / 2
-        val listHeight = (records.size.dp.value * itemHeight.value).coerceAtMost(maxHeight.value)
 
-        val scrollState = rememberLazyListState()
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(listHeight.dp)
         ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentHeight()
-                    .heightIn(screenHeight / 2),
-                state = scrollState,
+            Column(
+                modifier = Modifier,
                 verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
                 if (records.isEmpty()) {
-                    item {
-                        Text(text = stringResource(id = R.string.uart_output_placeholder))
-                    }
+                    Text(text = stringResource(id = R.string.uart_output_placeholder))
                 } else {
-                    items(records) { record ->
-                        when (record.type) {
-                            UARTRecordType.INPUT -> MessageItemInput(record = record)
-                            UARTRecordType.OUTPUT -> MessageItemOutput(record = record)
+                    // Display the latest messages at the start
+                    records.reversed().forEach {
+                        when (it.type) {
+                            UARTRecordType.INPUT -> MessageItemInput(record = it)
+                            UARTRecordType.OUTPUT -> MessageItemOutput(record = it)
                         }
                     }
                 }
-            }
-
-            // Automatically scroll to the latest item when a new record appears
-            LaunchedEffect(records) {
-                if (!scrollState.canScrollForward || records.isEmpty()) {
-                    return@LaunchedEffect
-                }
-                scrollState.animateScrollToItem(records.lastIndex)
             }
         }
     }
