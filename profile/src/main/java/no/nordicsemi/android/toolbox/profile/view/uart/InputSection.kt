3a50fcef220aaa3b0fbfce4314e246383039c8c7
hiar <aryal.himali@gmail.com>
2025-02-17 13:49:46 +0100

    Fixed input section.

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/InputSection.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/InputSection.kt
@@ -1,34 +1,48 @@
 package no.nordicsemi.android.toolbox.profile.view.uart
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imeNestedScroll
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import no.nordicsemi.android.common.ui.view.RadioButtonGroup
 import no.nordicsemi.android.common.ui.view.RadioButtonItem
 import no.nordicsemi.android.common.ui.view.RadioGroupViewEntity
@@ -39,44 +53,65 @@ import no.nordicsemi.android.toolbox.profile.viewmodel.UARTEvent
 import no.nordicsemi.android.ui.view.ScreenSection
 import no.nordicsemi.android.ui.view.SectionTitle
 
-@OptIn(ExperimentalLayoutApi::class)
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun InputSection(onEvent: (DeviceConnectionViewEvent) -> Unit) {
     var text by rememberSaveable { mutableStateOf("") }
     val checkedItem by rememberSaveable { mutableStateOf(MacroEol.entries[0]) }
+    val focusRequester = remember { FocusRequester() }
+    val bringIntoViewRequester = remember { BringIntoViewRequester() }
+    val coroutineScope = rememberCoroutineScope()
+    val focusManager = LocalFocusManager.current
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .imePadding() // Pushes up when keyboard appears
-            .imeNestedScroll()
-            .padding(horizontal = 4.dp, vertical = 16.dp) // Extra padding for spacing,
+            .padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
     ) {
         Box(modifier = Modifier.weight(1f)) {
-            OutlinedTextField(
+            BasicTextField(
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .bringIntoViewRequester(bringIntoViewRequester)
+                    .onFocusChanged {
+                        if (it.isFocused) {
+                            coroutineScope.launch {
+                                bringIntoViewRequester.bringIntoView()
+                            }
+                        }
+                    },
                 value = text,
-                label = { Text(stringResource(id = R.string.uart_input_hint)) },
-                onValueChange = { newValue: String ->
+                textStyle = LocalTextStyle.current.copy(color = LocalContentColor.current),
+                onValueChange = { newValue ->
                     text = newValue
-                }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
             )
-        }
-
-        IconButton(
-            onClick = {
-                onEvent(UARTEvent.OnRunInput(text, checkedItem))
-                text = ""
+            if (text.isEmpty()) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterStart),
+                    text = stringResource(id = R.string.uart_input_hint),
+                )
             }
-        ) {
-            Icon(
-                Icons.AutoMirrored.Filled.Send,
-                contentDescription = stringResource(id = R.string.uart_input_macro),
-            )
         }
+        Icon(
+            Icons.AutoMirrored.Filled.Send,
+            contentDescription = stringResource(id = R.string.uart_input_macro),
+            modifier = Modifier
+                .clip(CircleShape)
+                .clickable {
+                    if (text.isNotEmpty()) {
+                        onEvent(UARTEvent.OnRunInput(text, checkedItem))
+                        focusManager.clearFocus()
+                        text = ""
+                    }
+                }
+                .padding(8.dp)
+        )
     }
 }
 
