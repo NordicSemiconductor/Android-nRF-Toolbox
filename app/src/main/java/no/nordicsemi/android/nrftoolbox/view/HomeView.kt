15df2e00ab858c5a9da7b5ea567e33cbf06f604d
hiar <aryal.himali@gmail.com>
2025-03-13 13:28:40 +0100

    Removed any permission from home view.

--- app/src/main/java/no/nordicsemi/android/nrftoolbox/view/HomeView.kt
+++ app/src/main/java/no/nordicsemi/android/nrftoolbox/view/HomeView.kt
@@ -24,18 +24,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import no.nordicsemi.android.common.permissions.ble.RequireBluetooth
-import no.nordicsemi.android.common.permissions.notification.RequestNotificationPermission
 import no.nordicsemi.android.nrftoolbox.R
 import no.nordicsemi.android.nrftoolbox.viewmodel.HomeViewEvent
 import no.nordicsemi.android.nrftoolbox.viewmodel.HomeViewModel
 import no.nordicsemi.android.toolbox.profile.data.Profile
-
-private const val DFU_PACKAGE_NAME = "no.nordicsemi.android.dfu"
-private const val DFU_LINK =
-    "https://play.google.com/store/apps/details?id=no.nordicsemi.android.dfu"
-
-private const val LOGGER_PACKAGE_NAME = "no.nordicsemi.android.log"
 
 @Composable
 internal fun HomeView() {
@@ -67,117 +59,113 @@ internal fun HomeView() {
                 .padding(16.dp)
                 .fillMaxSize()
         ) {
-            RequireBluetooth {
-                RequestNotificationPermission {
-                    if (state.connectedDevices.isNotEmpty()) {
-                        Column(
-                            verticalArrangement = Arrangement.spacedBy(16.dp),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .verticalScroll(rememberScrollState())
-                        ) {
-                            Text(
-                                text = "Connected devices",
-                                style = MaterialTheme.typography.titleLarge,
-                            )
-                            state.connectedDevices.values.forEach { (peripheral, services) ->
-                                services.forEach { serviceManager ->
-                                    when (serviceManager.profile) {
-                                        Profile.HRS -> FeatureButton(
-                                            iconId = R.drawable.ic_hrs,
-                                            profileName = R.string.hrs_module_full,
+            if (state.connectedDevices.isNotEmpty()) {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(
+                        text = "Connected devices",
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                    state.connectedDevices.values.forEach { (peripheral, services) ->
+                        services.forEach { serviceManager ->
+                            when (serviceManager.profile) {
+                                Profile.HRS -> FeatureButton(
+                                    iconId = R.drawable.ic_hrs,
+                                    profileName = R.string.hrs_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.HTS -> FeatureButton(
+                                    iconId = R.drawable.ic_hts,
+                                    profileName = R.string.hts_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.BPS -> FeatureButton(
+                                    iconId = R.drawable.ic_bps,
+                                    profileName = R.string.bps_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.GLS -> FeatureButton(
+                                    iconId = R.drawable.ic_gls,
+                                    profileName = R.string.gls_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.CGM -> FeatureButton(
+                                    iconId = R.drawable.ic_cgm,
+                                    profileName = R.string.cgm_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.RSCS -> FeatureButton(
+                                    iconId = R.drawable.ic_rscs,
+                                    profileName = R.string.rscs_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.DFS -> FeatureButton(
+                                    iconId = R.drawable.ic_distance,
+                                    profileName = R.string.direction_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.CSC -> FeatureButton(
+                                    iconId = R.drawable.ic_csc,
+                                    profileName = R.string.cgm_module_full,
+                                    deviceName = peripheral.name,
+                                    true
+                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+
+                                Profile.BATTERY -> {
+                                    if (services.size == 1)
+                                        FeatureButton(
+                                            iconId = R.drawable.ic_battery,
+                                            profileName = R.string.battery_module_full,
                                             deviceName = peripheral.name,
                                             true
                                         ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+                                }
 
-                                        Profile.HTS -> FeatureButton(
-                                            iconId = R.drawable.ic_hts,
-                                            profileName = R.string.hts_module_full,
-                                            deviceName = peripheral.name,
-                                            true
-                                        ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+                                Profile.THROUGHPUT -> {
+                                    FeatureButton(
+                                        iconId = Icons.Default.SyncAlt,
+                                        name = "Throughput",
+                                        isRunning = true,
+                                        description = peripheral.name
+                                    ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+                                }
 
-                                        Profile.BPS -> FeatureButton(
-                                            iconId = R.drawable.ic_bps,
-                                            profileName = R.string.bps_module_full,
-                                            deviceName = peripheral.name,
-                                            true
-                                        ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+                                Profile.UART -> {
+                                    FeatureButton(
+                                        iconId = R.drawable.ic_uart,
+                                        profileName = R.string.uart_module_full,
+                                        deviceName = peripheral.name,
+                                        true
+                                    ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
+                                }
 
-                                        Profile.GLS -> FeatureButton(
-                                            iconId = R.drawable.ic_gls,
-                                            profileName = R.string.gls_module_full,
-                                            deviceName = peripheral.name,
-                                            true
-                                        ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-
-                                        Profile.CGM -> FeatureButton(
-                                            iconId = R.drawable.ic_cgm,
-                                            profileName = R.string.cgm_module_full,
-                                            deviceName = peripheral.name,
-                                            true
-                                        ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-
-                                        Profile.RSCS -> FeatureButton(
-                                            iconId = R.drawable.ic_rscs,
-                                            profileName = R.string.rscs_module_full,
-                                            deviceName = peripheral.name,
-                                            true
-                                        ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-
-                                        Profile.DFS -> FeatureButton(
-                                            iconId = R.drawable.ic_distance,
-                                            profileName = R.string.direction_module_full,
-                                            deviceName = peripheral.name,
-                                            true
-                                        ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-
-                                        Profile.CSC -> FeatureButton(
-                                            iconId = R.drawable.ic_csc,
-                                            profileName = R.string.cgm_module_full,
-                                            deviceName = peripheral.name,
-                                            true
-                                        ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-
-                                        Profile.BATTERY -> {
-                                            if (services.size == 1)
-                                                FeatureButton(
-                                                    iconId = R.drawable.ic_battery,
-                                                    profileName = R.string.battery_module_full,
-                                                    deviceName = peripheral.name,
-                                                    true
-                                                ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-                                        }
-
-                                        Profile.THROUGHPUT -> {
-                                            FeatureButton(
-                                                iconId = Icons.Default.SyncAlt,
-                                                name = "Throughput",
-                                                isRunning = true,
-                                                description = peripheral.name
-                                            ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-                                        }
-
-                                        Profile.UART -> {
-                                            FeatureButton(
-                                                iconId = R.drawable.ic_uart,
-                                                profileName = R.string.uart_module_full,
-                                                deviceName = peripheral.name,
-                                                true
-                                            ) { onEvent(HomeViewEvent.OnDeviceClick(peripheral.address)) }
-                                        }
-
-                                        else -> {
-                                            // TODO: Add more profiles
-                                        }
-                                    }
+                                else -> {
+                                    // TODO: Add more profiles
                                 }
                             }
                         }
-                    } else {
-                        NoConnectedDeviceView()
                     }
                 }
+            } else {
+                NoConnectedDeviceView()
             }
         }
     }
