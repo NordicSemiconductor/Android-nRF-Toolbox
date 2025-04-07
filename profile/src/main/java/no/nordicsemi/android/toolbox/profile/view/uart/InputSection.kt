e8b88acbff7c36859bae7d1677779b7c26d6df74
hiar <aryal.himali@gmail.com>
2025-04-07 15:48:45 +0200

    Removed unused code block.

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/InputSection.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/InputSection.kt
@@ -4,12 +4,9 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.shape.CircleShape
@@ -19,7 +16,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -39,21 +35,15 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
-import no.nordicsemi.android.common.ui.view.RadioButtonGroup
-import no.nordicsemi.android.common.ui.view.RadioButtonItem
-import no.nordicsemi.android.common.ui.view.RadioGroupViewEntity
 import no.nordicsemi.android.toolbox.profile.R
 import no.nordicsemi.android.toolbox.profile.data.uart.MacroEol
 import no.nordicsemi.android.toolbox.profile.viewmodel.DeviceConnectionViewEvent
 import no.nordicsemi.android.toolbox.profile.viewmodel.UARTEvent
-import no.nordicsemi.android.ui.view.ScreenSection
-import no.nordicsemi.android.ui.view.SectionTitle
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -143,53 +133,4 @@ internal fun InputSection(onEvent: (DeviceConnectionViewEvent) -> Unit) {
 @Composable
 private fun InputSectionPreview() {
     InputSection(onEvent = {})
-}
-
-@Composable
-internal fun EditInputSection(onEvent: (DeviceConnectionViewEvent) -> Unit) {
-    val checkedItem = rememberSaveable { mutableStateOf(MacroEol.entries[0]) }
-
-    val items = MacroEol.entries.map {
-        RadioButtonItem(it.toString(), it == checkedItem.value)
-    }
-    val viewEntity = RadioGroupViewEntity(items)
-
-    ScreenSection {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            SectionTitle(
-                resId = R.drawable.ic_input,
-                title = stringResource(R.string.uart_input),
-                menu = {
-                    IconButton(onClick = { onEvent(UARTEvent.MacroInputSwitchClicked) }) {
-                        Icon(
-                            painterResource(id = R.drawable.ic_macro),
-                            contentDescription = stringResource(id = R.string.uart_input_macro),
-                        )
-                    }
-                }
-            )
-
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = stringResource(id = R.string.uart_macro_dialog_eol),
-                    style = MaterialTheme.typography.labelLarge
-                )
-
-                RadioButtonGroup(viewEntity) {
-                    val i = items.indexOf(it)
-                    checkedItem.value = MacroEol.entries.toTypedArray()[i]
-                }
-            }
-
-            Spacer(modifier = Modifier.size(16.dp))
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun EditInputSectionPreview() {
-    EditInputSection(onEvent = {})
 }
