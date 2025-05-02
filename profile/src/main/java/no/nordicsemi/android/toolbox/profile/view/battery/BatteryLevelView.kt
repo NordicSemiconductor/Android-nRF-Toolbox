4f312c0449ef518978abbcb9343d5c95e413e53b
hiar <aryal.himali@gmail.com>
2025-05-02 11:58:05 +0200

    Changed to nordic colors.

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/battery/BatteryLevelView.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/battery/BatteryLevelView.kt
@@ -55,6 +55,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import no.nordicsemi.android.common.theme.nordicGrass
+import no.nordicsemi.android.common.theme.nordicGreen
 import no.nordicsemi.android.ui.R
 import no.nordicsemi.android.ui.view.ScreenSection
 import no.nordicsemi.android.ui.view.SectionTitle
@@ -83,25 +85,25 @@ internal fun BatteryLevelView(batteryLevel: Int) {
 internal fun DynamicBatteryStatus(batteryLevel: Int = 40) {
     val (batteryIcon: ImageVector, color: Color) = when {
         batteryLevel > 95 -> {
-            Icons.Outlined.BatteryFull to Color.Green
+            Icons.Outlined.BatteryFull to nordicGreen
         } // Full Battery
         batteryLevel > 80 -> {
-            Icons.Outlined.Battery6Bar to Color.Green
+            Icons.Outlined.Battery6Bar to nordicGreen
         }
 
         batteryLevel > 70 -> {
-            Icons.Outlined.Battery5Bar to Color.Green
+            Icons.Outlined.Battery5Bar to nordicGreen
         } // Moderate Battery
         batteryLevel > 55 -> {
-            Icons.Outlined.Battery4Bar to Color.Green
+            Icons.Outlined.Battery4Bar to nordicGreen
         } // Moderate Battery
 
         batteryLevel > 40 -> {
-            Icons.Outlined.Battery3Bar to Color.Green
+            Icons.Outlined.Battery3Bar to nordicGreen
         } // Moderate Battery
 
         batteryLevel > 25 -> {
-            Icons.Outlined.Battery2Bar to Color(0xFFD0DF00) // MaterialTheme.colorScheme.nordicGreen
+            Icons.Outlined.Battery2Bar to nordicGrass
         } // Low Battery
 
         batteryLevel > 10 -> {
