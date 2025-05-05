dc6f7069b98877e108a548b326930f2e8358b7fd
hiar <aryal.himali@gmail.com>
2025-05-05 13:41:17 +0200

    channel sounding testing

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/repository/channelSounding/ChannelSoundingManager.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/repository/channelSounding/ChannelSoundingManager.kt
@@ -10,15 +10,21 @@ import android.ranging.RangingPreference
 import android.ranging.RangingPreference.DEVICE_ROLE_RESPONDER
 import android.ranging.RangingSession
 import android.ranging.ble.cs.BleCsRangingParams
+import android.ranging.oob.DeviceHandle
+import android.ranging.oob.OobResponderRangingConfig
+import android.ranging.oob.TransportHandle
 import android.ranging.raw.RawRangingDevice
 import android.ranging.raw.RawResponderRangingConfig
 import androidx.annotation.RequiresApi
 import dagger.hilt.android.qualifiers.ApplicationContext
-import no.nordicsemi.android.toolbox.profile.repository.channelSounding.RangingSessionCloseReason.Companion.getReason
 import no.nordicsemi.android.toolbox.profile.repository.channelSounding.RangingSessionStartTechnology.Companion.getTechnology
 import timber.log.Timber
+import java.util.UUID
+import java.util.concurrent.Executor
 import javax.inject.Inject
 import javax.inject.Singleton
+
+val CHANNEL_SOUND_SERVICE_UUID: UUID = UUID.fromString("0000185B-0000-1000-8000-00805F9B34FB")
 
 @SuppressLint("NewApi")
 @Singleton
@@ -32,12 +38,20 @@ internal class ChannelSoundingManager @Inject constructor(
 
     private val rangingSessionCallback = object : RangingSession.Callback {
         override fun onClosed(reason: Int) {
-            Timber.tag("BBB").d("RangingManager session closed with reason: ${getReason(reason)}")
+            Timber.tag("BBB").d(
+                "RangingManager session closed with reason: ${
+                    RangingSessionCloseReason.getReason(reason)
+                }"
+            )
         }
 
         override fun onOpenFailed(reason: Int) {
             Timber.tag("BBB")
-                .d("RangingManager session open failed with reason: $reason")
+                .d(
+                    "RangingManager session open failed with reason: ${
+                        RangingSessionFailedReason.getReason(reason)
+                    }"
+                )
         }
 
         override fun onOpened() {
@@ -50,8 +64,11 @@ internal class ChannelSoundingManager @Inject constructor(
         ) {
             val measurement = data.distance?.measurement
             val confidence = data.distance?.confidence
+            Timber.tag("BBB").d("onResults rangingTechnology: ${data.rangingTechnology}")
+            Timber.tag("BBB").d("onResults azimuth: ${data.azimuth}")
+            Timber.tag("BBB").d("onResults elevation: ${data.elevation}")
             Timber.tag("BBB").d("RangingManager session results with peer: ${peer.uuid}")
-            Timber.tag("BBB").d("distance ${data.distance}\n angle: ${data.rssi} ")
+            Timber.tag("BBB").d("distance ${data.distance}\n rssi: ${data.rssi} ")
             Timber.tag("BBB").d("measurement: $measurement")
             Timber.tag("BBB").d("confidence: $confidence")
         }
@@ -75,6 +92,7 @@ internal class ChannelSoundingManager @Inject constructor(
         }
     }
 
+    @OptIn(ExperimentalStdlibApi::class)
     @RequiresApi(Build.VERSION_CODES.BAKLAVA)
     fun addDeviceToRangingSession(
         device: String
@@ -83,43 +101,76 @@ internal class ChannelSoundingManager @Inject constructor(
             Timber.tag("AAA").d("RangingManager is not available")
             return
         }
-        val rangingDevice = RangingDevice.Builder().build()
+        val rangingCapabilityCallback = RangingManager.RangingCapabilitiesCallback {
+            if (it.csCapabilities != null) {
+                Timber.tag("CCC").d("CS Capabilities: ${it.csCapabilities}")
+                it.csCapabilities!!.supportedSecurityLevels
+            } else {
+                Timber.tag("CCC").d("CS Capabilities is not supported")
+            }
 
-        val bleCsRangingParams = BleCsRangingParams.Builder(device).build()
+        }
+        rangingManager.registerCapabilitiesCallback(
+            context.mainExecutor,
+            rangingCapabilityCallback
+        )
+
+        val rangingDevice = RangingDevice.Builder()
+            .build()
+
+
+        val transportHandle = object : TransportHandle {
+            override fun close() {
+                Timber.tag("AAA").d("TransportHandle close")
+            }
+
+            override fun registerReceiveCallback(
+                executor: Executor,
+                callback: TransportHandle.ReceiveCallback
+            ) {
+                Timber.tag("AAA").d("TransportHandle registerReceiveCallback")
+            }
+
+            override fun sendData(data: ByteArray) {
+                Timber.tag("AAA").d("TransportHandle sendData: ${data.toHexString()}")
+            }
+        }
+
+        val oobResponderRangingConfig = OobResponderRangingConfig.Builder(
+            DeviceHandle.Builder(rangingDevice, transportHandle).build()
+        )
+            .build()
 
         val rawRangingDevice = RawRangingDevice.Builder()
-            .setCsRangingParams(bleCsRangingParams)
-            .setRangingDevice(rangingDevice)
-            .build()
+            .setCsRangingParams(BleCsRangingParams.Builder(device).build())
+            .setRangingDevice(rangingDevice).build()
 
-        val rawResponderRangingConfig = RawResponderRangingConfig.Builder()
-            .setRawRangingDevice(rawRangingDevice)
-            .build()
+        val rawRangingDeviceConfig =
+            RawResponderRangingConfig.Builder().setRawRangingDevice(rawRangingDevice).build()
 
         val rangingPreference = RangingPreference.Builder(
             DEVICE_ROLE_RESPONDER,
-            rawResponderRangingConfig
+            rawRangingDeviceConfig
         ).build()
 
-        Timber.tag("AAA").d("rangingPreference: $rangingPreference")
-        Timber.tag("AAA").d("rawResponderRangingConfig: $rawResponderRangingConfig")
-        Timber.tag("AAA").d("rawRangingDevice: $rawRangingDevice")
-        Timber.tag("AAA").d("bleCsRangingParams: $bleCsRangingParams")
         Timber.tag("AAA").d("rangingSessionCallback: $rangingSessionCallback")
+        Timber.tag("AAA").d("rangingPreference: $rangingPreference")
         Timber.tag("AAA").d("rangingDevice: $rangingDevice")
 
+        if (rangingSession != null) {
+            Timber.tag("AAA").d("RangingManager session is already started")
+            return
+        }
         rangingSession = rangingManager.createRangingSession(
             context.mainExecutor,
             rangingSessionCallback
         )
+
         if (rangingSession == null) {
             Timber.tag("AAA").d("RangingManager session is null")
             return
         }
-        /* // first remove the device if it exists
-         rangingSession!!.removeDeviceFromRangingSession(rangingDevice)
-         sleep(2000)*/
-        rangingSession!!.addDeviceToRangingSession(rawResponderRangingConfig)
+        rangingSession!!.addDeviceToRangingSession(rawRangingDeviceConfig)
         rangingSession!!.start(rangingPreference)
     }
 
