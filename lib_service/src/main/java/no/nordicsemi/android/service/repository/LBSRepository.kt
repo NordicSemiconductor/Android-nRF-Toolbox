1e286512f0cbab31e17a4aab1822885085d1fbf6
himalia416 <himali.aryal@nordicsemi.no>
2025-05-30 09:46:58 +0200

    Read/write data to LBS

--- lib_service/src/main/java/no/nordicsemi/android/service/repository/LBSRepository.kt
+++ lib_service/src/main/java/no/nordicsemi/android/service/repository/LBSRepository.kt
@@ -7,19 +7,39 @@ import no.nordicsemi.android.toolbox.profile.data.LBSServiceData
 data object LBSRepository {
     private val _dataMap = mutableMapOf<String, MutableStateFlow<LBSServiceData>>()
 
+    /**
+     * Returns a [MutableStateFlow] that holds the [LBSServiceData] for the given device ID.
+     * If no data exists for the device ID, it initializes a new [MutableStateFlow] with an empty [LBSServiceData].
+     */
     fun getData(deviceId: String): MutableStateFlow<LBSServiceData> {
         return _dataMap.getOrPut(deviceId) { MutableStateFlow(LBSServiceData()) }
     }
 
+    /**
+     * Updates the LED state for the given device ID.
+     * If the device ID does not exist, it will not perform any action.
+     */
     fun updateLEDState(deviceId: String, ledState: Boolean) {
         _dataMap[deviceId]?.update {
             it.copy(data = it.data?.copy(ledOn = ledState))
         }
     }
 
+    /**
+     * Updates the button state for the given device ID.
+     * If the device ID does not exist, it will not perform any action.
+     */
     fun updateButtonState(deviceId: String, buttonState: Boolean) {
         _dataMap[deviceId]?.update {
             it.copy(data = it.data?.copy(buttonPressed = buttonState))
         }
+    }
+
+    /**
+     * Clears the data for the given device ID.
+     * This will remove the [MutableStateFlow] associated with the device ID from the repository.
+     */
+    fun clear(deviceId: String) {
+        _dataMap.remove(deviceId)
     }
 }
