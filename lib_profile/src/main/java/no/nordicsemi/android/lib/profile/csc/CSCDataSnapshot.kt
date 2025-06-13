2f73f9fd177c747fa7027cf9fefd0b88eaf22cd7
hiar <aryal.himali@gmail.com>
2025-06-13 14:40:24 +0200

    Cadence data parser fixes

--- lib_profile/src/main/java/no/nordicsemi/android/lib/profile/csc/CSCDataSnapshot.kt
+++ lib_profile/src/main/java/no/nordicsemi/android/lib/profile/csc/CSCDataSnapshot.kt
@@ -1,8 +1,8 @@
 package no.nordicsemi.android.lib.profile.csc
 
 internal data class CSCDataSnapshot(
-    val wheelRevolutions: Long? = null,
-    val wheelEventTime: Int? = null,
-    val crankRevolutions: Long? = null,
-    val crankEventTime: Int? = null,
+    var wheelRevolutions: Long = -1,
+    var wheelEventTime: Int = -1,
+    var crankRevolutions: Long = -1,
+    var crankEventTime: Int = -1
 )
