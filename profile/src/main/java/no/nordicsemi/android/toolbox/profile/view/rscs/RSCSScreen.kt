7989cf14454f759d980e430ae6b6adcbfd3e8f0b
himalia416 <himali.aryal@nordicsemi.no>
2025-06-16 09:54:51 +0200

    Fixed rscs view

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/rscs/RSCSScreen.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/rscs/RSCSScreen.kt
@@ -2,16 +2,18 @@ package no.nordicsemi.android.toolbox.profile.view.rscs
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,8 +23,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import no.nordicsemi.android.lib.profile.rscs.RSCFeatureData
 import no.nordicsemi.android.lib.profile.rscs.RSCSSettingsUnit
 import no.nordicsemi.android.toolbox.profile.R
@@ -56,10 +61,7 @@ internal fun RSCSScreen(
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp)
             ) {
                 SectionRow {
-                    KeyValueColumn(
-                        stringResource(id = R.string.rscs_cadence),
-                        serviceData.displayPace()
-                    )
+                    KeyValueColumn(stringResource(id = R.string.rscs_cadence), serviceData.displayPace())
                     KeyValueColumnReverse(
                         value = stringResource(id = R.string.rscs_activity),
                         key = if (serviceData.data.running)
@@ -69,15 +71,10 @@ internal fun RSCSScreen(
                 }
                 SectionRow {
                     KeyValueColumn("Speed", "${serviceData.displaySpeed()}")
-                    serviceData.displayNumberOfSteps()?.let {
-                        KeyValueColumnReverse(
-                            stringResource(id = R.string.rscs_number_of_steps),
-                            it
-                        )
-                    } ?: serviceData.displayStrideLength()?.let {
-                        KeyValueColumnReverse(
-                            stringResource(id = R.string.stride_length), it
-                        )
+                    serviceData.displayStrideLength()?.let {
+                        KeyValueColumnReverse(stringResource(id = R.string.stride_length), it)
+                    } ?: serviceData.displayNumberOfSteps()?.let {
+                        KeyValueColumnReverse(stringResource(id = R.string.rscs_number_of_steps), it)
                     }
                 }
             }
@@ -159,35 +156,59 @@ private fun RSCSSettingsDialog(
     onDismiss: () -> Unit,
     onSpeedUnitSelected: (RSCSEvent) -> Unit
 ) {
-    val speedUnitEntries = RSCSSettingsUnit.entries.map { it }
-
-    AlertDialog(
+    Dialog(
         onDismissRequest = { onDismiss() },
-        title = { Text(text = stringResource(R.string.rscs_settings_unit_title)) },
-        text = {
-            Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                speedUnitEntries.forEachIndexed { _, entry ->
-                    Text(
-                        text = entry.toString(),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                onSpeedUnitSelected(
-                                    RSCSEvent.OnSelectedSpeedUnitSelected(
-                                        entry
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true
+        )
+    ) {
+        OutlinedCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.csc_settings),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Column {
+                    RSCSSettingsUnit.entries.forEach { entry ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(4.dp))
+                                .clickable {
+                                    onSpeedUnitSelected(
+                                        RSCSEvent.OnSelectedSpeedUnitSelected(entry)
                                     )
-                                )
-                                onDismiss()
-                            },
-                        color = if (state.unit == entry)
-                            MaterialTheme.colorScheme.primary else
-                            MaterialTheme.colorScheme.onBackground
-                    )
+                                    onDismiss()
+                                },
+                        ) {
+                            Text(
+                                text = entry.toString(),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                color = if (state.unit == entry)
+                                    MaterialTheme.colorScheme.primary else
+                                    MaterialTheme.colorScheme.onBackground
+                            )
+                        }
+                    }
                 }
             }
-        },
-        confirmButton = {}
-    )
+        }
+    }
 }
 
 @Preview(showBackground = true)
