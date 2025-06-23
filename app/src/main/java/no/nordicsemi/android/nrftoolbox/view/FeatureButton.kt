75629ec5752001b28d5bd8aeefa1e8549f449ab0
himalia416 <himali.aryal@nordicsemi.no>
2025-06-23 15:15:16 +0200

    Fixed icon cutoff

--- app/src/main/java/no/nordicsemi/android/nrftoolbox/view/FeatureButton.kt
+++ app/src/main/java/no/nordicsemi/android/nrftoolbox/view/FeatureButton.kt
@@ -9,14 +9,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
@@ -47,7 +45,6 @@ internal fun FeatureButton(
                 colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
                 modifier = Modifier
                     .size(40.dp)
-                    .clip(CircleShape)
             )
 
             Column(
@@ -112,7 +109,6 @@ internal fun FeatureButton(
                 colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
                 modifier = Modifier
                     .size(40.dp)
-                    .clip(CircleShape)
             )
 
             Column(
