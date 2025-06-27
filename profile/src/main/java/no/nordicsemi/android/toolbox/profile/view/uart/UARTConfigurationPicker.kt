447b1e0383eacef7f9c2de37dc4a32ec38411e45
himalia416 <himali.aryal@nordicsemi.no>
2025-06-27 08:16:01 +0200

    UART: configuration fixed

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/UARTConfigurationPicker.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/uart/UARTConfigurationPicker.kt
@@ -1,16 +1,15 @@
 package no.nordicsemi.android.toolbox.profile.view.uart
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType.Companion.PrimaryNotEditable
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -23,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import no.nordicsemi.android.toolbox.profile.R
 import no.nordicsemi.android.toolbox.profile.data.UARTViewState
 import no.nordicsemi.android.toolbox.profile.data.uart.UARTConfiguration
@@ -46,20 +46,18 @@ internal fun UARTConfigurationPicker(
             modifier = Modifier.menuAnchor(PrimaryNotEditable)
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 val title =
                     state.selectedConfigurationName
                         ?: stringResource(id = R.string.uart_configuration_picker_hint)
-                Column {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.labelSmall
-                    )
-                }
-
+                Text(
+                    text = title
+                )
                 Icon(Icons.Default.ArrowDropDown, contentDescription = "")
             }
         }
