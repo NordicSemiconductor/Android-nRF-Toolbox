e36df72f1d7491f70665afc2b5b51c46e197fd93
himalia416 <himali.aryal@nordicsemi.no>
2025-07-22 18:17:55 +0200

    AGP upgrade

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,7 +50,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:2.8.1")
+            from("no.nordicsemi.android.gradle:version-catalog:2.9")
         }
     }
 }
