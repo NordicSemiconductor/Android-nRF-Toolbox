f50bf9cb575a2b14c1261d552c37c1e207551377
himalia416 <himali.aryal@nordicsemi.no>
2025-07-25 15:26:42 +0200

    Removed unused code block

--- lib_utils/src/main/java/no/nordicsemi/android/toolbox/lib/utils/Ext.kt
+++ lib_utils/src/main/java/no/nordicsemi/android/toolbox/lib/utils/Ext.kt
@@ -1,20 +1,7 @@
 package no.nordicsemi.android.toolbox.lib.utils
 
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 fun Throwable.logAndReport() {
     Timber.e(this)
 }
-
-private val exceptionHandler = CoroutineExceptionHandler { _, t ->
-    t.logAndReport()
-}
-
-fun CoroutineScope.launchWithCatch(block: suspend CoroutineScope.() -> Unit) =
-    launch(SupervisorJob() + exceptionHandler) {
-        block()
-    }
