8a7eb67d72103d0a98c5d620254ec977c64a5eea
hiar <aryal.himali@gmail.com>
2025-08-13 16:17:35 +0200

    Migrated to new connection state

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/data/UiMapper.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/data/UiMapper.kt
@@ -5,7 +5,7 @@ import no.nordicsemi.kotlin.ble.core.ConnectionState.Disconnected.Reason
 fun toReason(reason:  Reason): String =
     when (reason) {
         Reason.Cancelled -> "Connection was cancelled."
-        Reason.LinkLoss -> "Device signal has been lost."
+        Reason.LinkLoss -> "The device got out of range or has turned off."
         Reason.Success -> "Device disconnected successfully."
         Reason.TerminateLocalHost -> "Device disconnected by the local host."
         Reason.TerminatePeerUser -> "Device disconnected by the peer user."
@@ -13,4 +13,5 @@ fun toReason(reason:  Reason): String =
         is Reason.Unknown -> "Oops...! Connection went on a coffee break."
         Reason.UnsupportedAddress -> "Device disconnected due to unsupported address."
         Reason.InsufficientAuthentication -> "Device disconnected due to insufficient authentication."
+        Reason.UnsupportedConfiguration -> "Connection attempt was aborted due PHY negotiations failure."
     }
