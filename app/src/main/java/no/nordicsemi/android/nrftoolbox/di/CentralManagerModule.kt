37b7c7ae818c676aee2b58a4156d399a80d88bbc
hiar <aryal.himali@gmail.com>
2025-09-01 16:42:11 +0200

    Single instance of CentralManagerModule

--- app/src/main/java/no/nordicsemi/android/nrftoolbox/di/CentralManagerModule.kt
+++ app/src/main/java/no/nordicsemi/android/nrftoolbox/di/CentralManagerModule.kt
@@ -9,12 +9,14 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import no.nordicsemi.kotlin.ble.client.android.CentralManager
 import no.nordicsemi.kotlin.ble.client.android.native
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object CentralManagerModule {
 
     @Provides
+    @Singleton
     fun provideCentralManager(
         @ApplicationContext context: Context,
         scope: CoroutineScope
