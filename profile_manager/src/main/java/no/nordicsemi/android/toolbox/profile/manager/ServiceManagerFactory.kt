1b50a49e9df9ce558f20e0b2ca340bbc93ef064d
hiar <aryal.himali@gmail.com>
2025-09-09 14:24:19 +0200

    DFU uuids

--- profile_manager/src/main/java/no/nordicsemi/android/toolbox/profile/manager/ServiceManagerFactory.kt
+++ profile_manager/src/main/java/no/nordicsemi/android/toolbox/profile/manager/ServiceManagerFactory.kt
@@ -5,12 +5,17 @@ import no.nordicsemi.android.toolbox.lib.utils.spec.BPS_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.CGMS_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.CHANNEL_SOUND_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.CSC_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.DFU_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.DF_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.EXPERIMENTAL_BUTTONLESS_DFU_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.GLS_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.HRS_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.HTS_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.LBS_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.LEGACY_DFU_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.MDS_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.RSCS_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.SMP_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.THROUGHPUT_SERVICE_UUID
 import no.nordicsemi.android.toolbox.lib.utils.spec.UART_SERVICE_UUID
 import kotlin.uuid.ExperimentalUuidApi
@@ -34,6 +39,12 @@ object ServiceManagerFactory {
         UART_SERVICE_UUID to ::UARTManager,
         CHANNEL_SOUND_SERVICE_UUID to ::ChannelSoundingManager,
         LBS_SERVICE_UUID to ::LBSManager,
+        DFU_SERVICE_UUID  to ::DFUManager,
+        SMP_SERVICE_UUID to ::DFUManager,
+        MDS_SERVICE_UUID to ::DFUManager,
+        LEGACY_DFU_SERVICE_UUID to ::DFUManager,
+        EXPERIMENTAL_BUTTONLESS_DFU_SERVICE_UUID to ::DFUManager,
+
         // Add more service UUIDs to handler mappings as needed
     ).mapKeys { it.key.toKotlinUuid() }
 
