b908b58b211089078f15b54b5b7b92313e8b1241
hiar <aryal.himali@gmail.com>
2025-09-15 15:00:06 +0200

    Updated ui, ranging update rate

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/channelSounding/ChannelSoundingScreen.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/channelSounding/ChannelSoundingScreen.kt
@@ -1,31 +1,34 @@
 package no.nordicsemi.android.toolbox.profile.view.channelSounding
 
-import android.content.pm.PackageManager
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.SocialDistance
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import no.nordicsemi.android.permissions_ranging.RequestRangingPermission
+import no.nordicsemi.android.toolbox.profile.data.ChannelSoundingServiceData
 import no.nordicsemi.android.toolbox.profile.data.directionFinder.Range
 import no.nordicsemi.android.toolbox.profile.view.directionFinder.DistanceChartView
 import no.nordicsemi.android.toolbox.profile.viewmodel.ChannelSoundingViewModel
+import no.nordicsemi.android.ui.view.ScreenSection
 import no.nordicsemi.android.ui.view.SectionTitle
+import no.nordicsemi.android.ui.view.TextWithAnimatedDots
+import no.nordicsemi.android.ui.view.animate.AnimatedDistance
 import java.util.Locale
 
 @Composable
@@ -35,100 +38,131 @@ internal fun ChannelSoundingScreen() {
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
         RequestRangingPermission {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxSize()
-                    .padding(16.dp)
-            ) {
+            ChannelSoundingView(channelSoundingState)
+        }
+    } else {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            ScreenSection(modifier = Modifier.padding(0.dp) /* No padding */) {
+                Column(modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)) {
+                    SectionTitle(
+                        icon = Icons.Default.SocialDistance,
+                        title = "Channel Sounding",
+                    )
+                    Text("Channel Sounding is not supported on this Android version.")
+                }
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.BAKLAVA)
+@Composable
+private fun ChannelSoundingView(channelSoundingState: ChannelSoundingServiceData) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        ScreenSection(modifier = Modifier.padding(0.dp) /* No padding */) {
+            Column(modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)) {
                 SectionTitle(
                     icon = Icons.Default.SocialDistance,
                     title = "Channel Sounding",
                 )
-                val context = LocalContext.current
-                val rangingPermissionStatusMessage =
-                    if (ContextCompat.checkSelfPermission(
-                            context,
-                            "android.permission.RANGING"
-                        ) == PackageManager.PERMISSION_GRANTED
-                    ) {
-                        "Ranging permission is granted"
-                    } else {
-                        "Ranging permission is not granted"
-                    }
-
+            }
+            if (channelSoundingState.rangingData == null) {
+                Spacer(modifier = Modifier.padding(bottom = 16.dp))
+                TextWithAnimatedDots(
+                    text = "Initiating ranging",
+                    modifier = Modifier.padding(16.dp)
+                )
+                Text("Please wait", modifier = Modifier.padding(16.dp))
+            }
+            channelSoundingState.rangingData?.let { rangingData ->
+                val measurement = rangingData.distance?.measurement
+                val confidence = rangingData.distance?.confidence
                 Column(
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp)
+                        .padding(16.dp),
                 ) {
-                    Text(rangingPermissionStatusMessage)
-                    channelSoundingState.rangingData?.let { rangingData ->
-                        val measurement = rangingData.distance?.measurement
-                        val confidence = rangingData.distance?.confidence
+                    measurement?.let {
+                        val distance = String.format(Locale.US, "%.2f", it)
+                        Text("Distance: $distance m")
+                    }
+                    confidence?.let {
+                        Text("Confidence: ${ConfidenceLevel.displayString(it)}")
+                    }
+                    Text(
+                        "RangingTechnology: ${
+                            RangingTechnology.displayString(
+                                rangingData.rangingTechnology
+                            )
+                        }"
+                    )
+                    measurement?.let {
+                        val rangeMax =
+                            if (it < 5) 5
+                            else if (it < 10) 10
+                            else if (it < 20) 20
+                            else if (it < 50) 50
+                            else if (it < 100) 100
+                            else if (it < 200) 200
+                            else if (it < 500) 500
+                            else 1000
 
-                        Text(
-                            text = "Distance: ${if (measurement != null) "$measurement m" else "null"}, " + "\n" +
-                                    "Confidence: ${
-                                        if (confidence != null)
-                                            "${ConfidenceLevel.displayString(confidence)} " else "null"
-                                    }" + "\n" +
-                                    "RangingTechnology: ${
-                                        RangingTechnology.displayString(
-                                            rangingData.rangingTechnology
-                                        )
-                                    }\n" +
-                                    "Azimuth: ${rangingData.azimuth}\nElevation: ${rangingData.elevation}"
-                        )
 
-                        measurement?.let {
-                            val rangeMax =
-                                if (it < 20) 20
-                                else if (it < 50) 50
-                                else if (it < 100) 100
-                                else if (it < 200) 200
-                                else if (it < 500) 500
-                                else 1000
+                        DistanceChartView(value = it.toInt(), range = Range(0, rangeMax))
+                        Spacer(modifier = Modifier.padding(4.dp))
 
-                            Column {
-                                DistanceChartView(value = it.toInt(), range = Range(0, rangeMax))
-                                Spacer(modifier = Modifier.padding(4.dp))
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(text = String.format(Locale.US, "%d m", 0))
 
-                                Row(
-                                    horizontalArrangement = Arrangement.SpaceBetween,
-                                    modifier = Modifier.fillMaxWidth()
-                                ) {
-                                    Text(text = String.format(Locale.US, "%d m", 0))
+                            val diff = rangeMax - 0
+                            val part = (diff / 4)
+                            Text(text = String.format(Locale.US, "%d m", 0 + part))
+                            Text(text = String.format(Locale.US, "%d m", 0 + 2 * part))
 
-                                    val diff = rangeMax - 0
-                                    val part = (diff / 4)
-                                    Text(text = String.format(Locale.US, "%d m", 0 + part))
-                                    Text(text = String.format(Locale.US, "%d m", 0 + 2 * part))
+                            Text(text = String.format(Locale.US, "%d m", rangeMax))
+                        }
 
-                                    Text(text = String.format(Locale.US, "%d m", rangeMax))
-                                }
-                            }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            AnimatedDistance(
+                                modifier = Modifier.padding(8.dp),
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                            val distance = String.format(Locale.US, "%.2f", measurement)
+                            Text(
+                                text = " Distance $distance m",
+                                style = MaterialTheme.typography.titleLarge,
+                            )
                         }
                     }
                 }
             }
         }
-    } else {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxSize()
-                .padding(16.dp)
-        ) {
-            Text("Channel Sounding is not supported on this Android version.")
-        }
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.BAKLAVA)
+@Preview(showBackground = true)
+@Composable
+private fun ChannelSoundingViewPreview() {
+    val sampleData = ChannelSoundingServiceData(
+        rangingData = null
+    )
+    ChannelSoundingView(sampleData)
 }
 
 internal enum class RangingTechnology(val value: Int) {
