40aaea3c235015d716227dbe2e31c3100a07a2d4
himalia416 <himali.aryal@nordicsemi.no>
2025-09-26 16:40:17 +0200

    Session closed message

--- profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/channelSounding/ChannelSoundingUiMapper.kt
+++ profile/src/main/java/no/nordicsemi/android/toolbox/profile/view/channelSounding/ChannelSoundingUiMapper.kt
@@ -2,7 +2,10 @@ package no.nordicsemi.android.toolbox.profile.view.channelSounding
 
 import androidx.annotation.StringRes
 import no.nordicsemi.android.toolbox.profile.R
+import no.nordicsemi.android.toolbox.profile.data.RangingSessionFailedReason
 import no.nordicsemi.android.toolbox.profile.data.RangingTechnology
+import no.nordicsemi.android.toolbox.profile.data.SessionCloseReasonProvider
+import no.nordicsemi.android.toolbox.profile.data.SessionClosedReason
 import no.nordicsemi.android.toolbox.profile.data.UpdateRate
 import no.nordicsemi.android.toolbox.profile.data.UpdateRate.FREQUENT
 import no.nordicsemi.android.toolbox.profile.data.UpdateRate.INFREQUENT
@@ -36,5 +39,22 @@ internal fun RangingTechnology.toUiString(): Int {
         RangingTechnology.WIFI_STA_RTT -> R.string.ranging_tech_wifi_sta_rtt
     }
 
+}
+
+@StringRes
+internal fun SessionCloseReasonProvider.toUiString(): Int {
+    return when (this) {
+        SessionClosedReason.MISSING_PERMISSION -> R.string.cs_missing_permissions
+        SessionClosedReason.NOT_SUPPORTED -> R.string.channel_sounding_not_supported
+        SessionClosedReason.RANGING_NOT_AVAILABLE -> R.string.cs_ranging_not_available
+        SessionClosedReason.CS_SECURITY_NOT_AVAILABLE -> R.string.cs_security_not_available
+        SessionClosedReason.UNKNOWN -> R.string.cs_error_unknown
+        RangingSessionFailedReason.LOCAL_REQUEST -> R.string.cs_local_request
+        RangingSessionFailedReason.REMOTE_REQUEST -> R.string.cs_remote_request
+        RangingSessionFailedReason.UNSUPPORTED -> R.string.cs_params_unsupported
+        RangingSessionFailedReason.SYSTEM_POLICY -> R.string.cs_system_policy
+        RangingSessionFailedReason.NO_PEERS_FOUND -> R.string.cs_no_peers_found
+        RangingSessionFailedReason.UNKNOWN -> R.string.cs_error_unknown
+    }
 }
 
