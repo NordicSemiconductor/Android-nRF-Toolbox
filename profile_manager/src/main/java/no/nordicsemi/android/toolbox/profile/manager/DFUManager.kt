606d040a4f84d99ecab70ad89f7f409a033f80b4
himalia416 <himali.aryal@nordicsemi.no>
2025-09-30 18:03:54 +0200

    Added DFU service

--- profile_manager/src/main/java/no/nordicsemi/android/toolbox/profile/manager/DFUManager.kt
+++ profile_manager/src/main/java/no/nordicsemi/android/toolbox/profile/manager/DFUManager.kt
@@ -2,18 +2,33 @@ package no.nordicsemi.android.toolbox.profile.manager
 
 import kotlinx.coroutines.CoroutineScope
 import no.nordicsemi.android.toolbox.lib.utils.Profile
+import no.nordicsemi.android.toolbox.lib.utils.spec.DFU_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.EXPERIMENTAL_BUTTONLESS_DFU_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.LEGACY_DFU_SERVICE_UUID
+import no.nordicsemi.android.toolbox.lib.utils.spec.SMP_SERVICE_UUID
 import no.nordicsemi.kotlin.ble.client.RemoteService
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.toKotlinUuid
 
 internal class DFUManager :ServiceManager{
     override val profile: Profile
         get() = Profile.DFU
 
+    @OptIn(ExperimentalUuidApi::class)
     override suspend fun observeServiceInteractions(
         deviceId: String,
         remoteService: RemoteService,
         scope: CoroutineScope
     ) {
-        // No interactions to observe for DFU profile
+        when (remoteService.uuid) {
+            DFU_SERVICE_UUID.toKotlinUuid(),
+            SMP_SERVICE_UUID.toKotlinUuid(),
+            LEGACY_DFU_SERVICE_UUID.toKotlinUuid(),
+            EXPERIMENTAL_BUTTONLESS_DFU_SERVICE_UUID.toKotlinUuid() -> this
+
+            else -> null
+
+        }
     }
 
 }
