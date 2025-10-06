25e1de5497d6cb3a2f2417ba1a90d06ad0f595ce
himalia416 <himali.aryal@nordicsemi.no>
2025-10-06 16:58:54 +0200

    use string resource

--- profile_data/src/main/java/no/nordicsemi/android/toolbox/profile/data/DFUServiceData.kt
+++ profile_data/src/main/java/no/nordicsemi/android/toolbox/profile/data/DFUServiceData.kt
@@ -1,6 +1,7 @@
 package no.nordicsemi.android.toolbox.profile.data
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import no.nordicsemi.android.toolbox.lib.utils.Profile
 import no.nordicsemi.android.toolbox.lib.utils.R
 
@@ -20,37 +21,43 @@ data class DFUServiceData(
 enum class DFUsAvailable(
     val packageName: String,
     val appLink: String,
-    val appName: String,
-    @DrawableRes val appIcon: Int
+    @param:StringRes val appName: Int,
+    @param:DrawableRes val appIcon: Int,
+    @param:StringRes val appShortName: Int,
 ) {
     DFU_SERVICE(
         packageName = DFU_PACKAGE_NAME,
         appLink = DFU_APP_LINK,
-        appName = "DFU",
-        appIcon = R.drawable.ic_dfu
+        appName = R.string.dfu_app_name,
+        appIcon = R.drawable.ic_dfu,
+        appShortName = R.string.dfu_short_name,
     ),
     SMP_SERVICE(
         packageName = SMP_PACKAGE_NAME,
         appLink = SMP_APP_LINK,
-        appName = "nRF Connect Device Manager",
-        appIcon = R.drawable.ic_device_manager
+        appName = R.string.smp_app_name,
+        appIcon = R.drawable.ic_device_manager,
+        appShortName = R.string.smp_short_name,
     ),
     MDS_SERVICE(
         packageName = SMP_PACKAGE_NAME,
         appLink = SMP_APP_LINK,
-        appName = "nRF Connect Device Manager",
-        appIcon = R.drawable.ic_device_manager
+        appName = R.string.mds_app_name,
+        appIcon = R.drawable.ic_device_manager,
+        appShortName = R.string.mds_app_name,
     ),
     LEGACY_DFU_SERVICE(
         packageName = DFU_PACKAGE_NAME,
         appLink = DFU_APP_LINK,
-        appName = "DFU",
-        appIcon = R.drawable.ic_dfu
+        appName = R.string.legacy_dfu_app_name,
+        appIcon = R.drawable.ic_dfu,
+        appShortName = R.string.legacy_dfu_short_name,
     ),
     EXPERIMENTAL_BUTTONLESS_DFU_SERVICE(
         packageName = DFU_PACKAGE_NAME,
         appLink = DFU_APP_LINK,
-        appName = "DFU",
-        appIcon = R.drawable.ic_dfu
+        appName = R.string.buttonless_dfu_app_name,
+        appIcon = R.drawable.ic_dfu,
+        appShortName = R.string.buttonless_dfu_short_name,
     )
 }
